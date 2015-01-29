3c9a56a92391934cfd09db5cdb09c8f336b26c78
jeaye <contact@jeaye.com>
2015-01-29 22:22:40 +0800

    Add file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
 
-int main()
+#include <jtl/iterator/stream_delim.hpp>
+#include <jtl/iterator/back_insert.hpp>
+
+int main(int const argc, char ** const argv)
 {
+  if(argc != 2)
+  {
+    std::cout << "usage: " << argv[0] << " <file>" << std::endl;
+    return 1;
+  }
+
+  std::ifstream ifs{ argv[1] };
+  std::vector<std::string> file;
+  std::copy(jtl::it::stream_delim<>(ifs, ""),
+            jtl::it::stream_delim<>(),
+            jtl::it::back_inserter(file));
+  std::string contents{ file.at(0) };
+
+  std::cout << contents << std::endl;
 }
