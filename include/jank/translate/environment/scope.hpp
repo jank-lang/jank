a1d9dd0a80a113dc7dc6d748cda2bfe10298e2f4
jeaye <contact@jeaye.com>
2015-05-04 23:30:16 +0800

    Refactor out environment/scope

--- include/jank/translate/environment/scope.hpp
+++ include/jank/translate/environment/scope.hpp
@@ -6,7 +6,6 @@
 #include <experimental/optional>
 
 #include <jank/translate/cell/cell.hpp>
-#include <jank/translate/expect/type.hpp>
 
 namespace jank
 {
@@ -22,22 +21,7 @@ namespace jank
         { }
 
         std::experimental::optional<std::vector<cell::function_definition>> find_function
-        (std::string const &name)
-        {
-          auto const it(function_definitions.find(name));
-          if(it == function_definitions.end())
-          {
-            if(parent)
-            { return parent->find_function(name); }
-            else
-            { return {}; }
-          }
-
-          if(it->second.empty())
-          { throw expect::error::type::type<>{ "unknown function: " + name }; }
-
-          return { it->second };
-        }
+        (std::string const &name);
 
         std::map<std::string, std::vector<cell::function_definition>> function_definitions;
         std::shared_ptr<scope> parent{ std::make_shared<scope>() };
---
+++ src/jank/translate/environment/scope.cpp
@@ -0,0 +1,30 @@
+#include <jank/translate/environment/scope.hpp>
+
+#include <jank/translate/expect/type.hpp>
+
+namespace jank
+{
+  namespace translate
+  {
+    namespace environment
+    {
+      std::experimental::optional<std::vector<cell::function_definition>> scope::find_function
+      (std::string const &name)
+      {
+        auto const it(function_definitions.find(name));
+        if(it == function_definitions.end())
+        {
+          if(parent)
+          { return parent->find_function(name); }
+          else
+          { return {}; }
+        }
+
+        if(it->second.empty())
+        { throw expect::error::type::type<>{ "unknown function: " + name }; }
+
+        return { it->second };
+      }
+    }
+  }
+}
