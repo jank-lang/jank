685d1d2bf245bb12e14334805b300faa4de05d32
jeaye <contact@jeaye.com>
2015-05-04 23:47:41 +0800

    Refactor out parse/cell/stream

--- include/jank/parse/cell/stream.hpp
+++ include/jank/parse/cell/stream.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+
 #include <jank/parse/cell/cell.hpp>
 
 namespace jank
@@ -8,49 +10,7 @@ namespace jank
   {
     namespace cell
     {
-      inline std::ostream& operator <<(std::ostream &os, cell const &c)
-      {
-        static int indent_level{ -1 };
-
-        switch(static_cast<type>(c.which()))
-        {
-          case type::boolean:
-            os << std::boolalpha << boost::get<boolean>(c).data;
-            break;
-          case type::integer:
-            os << boost::get<integer>(c).data;
-            break;
-          case type::real:
-            os << boost::get<real>(c).data;
-            break;
-          case type::string:
-            os << boost::get<string>(c).data;
-            break;
-          case type::ident:
-            os << "<" << boost::get<ident>(c).data << ">";
-            break;
-          case type::list:
-            ++indent_level;
-            os << "\n";
-            for(auto const i : jtl::it::make_range(0, indent_level))
-            { static_cast<void>(i); os << "  "; }
-
-            os << "( ";
-            for(auto const &v : boost::get<list>(c).data)
-            { os << v << " "; }
-            os << ") ";
-
-            --indent_level;
-            break;
-          case type::function:
-            os << "function ";
-            break;
-          default:
-            os << "??? ";
-        }
-
-        return os;
-      }
+      std::ostream& operator <<(std::ostream &os, cell const &c);
     }
   }
 }
