e257aba9899055ab0f7dbe0f773e578ba303ecb8
jeaye <contact@jeaye.com>
2015-05-24 18:18:19 +0800

    Add nested comment parsing tests

---
+++ test/include/parse/comment/nested.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <jest/jest.hpp>
+
+#include "common/translate.hpp"
+#include <jank/parse/expect/error/syntax/syntax.hpp>
+
+namespace jank
+{
+  struct nested_test
+  { };
+  using nested_group = jest::group<nested_test>;
+  static nested_group const nested_obj{ "comment nested" };
+}
+
+namespace jest
+{
+  template <> template <>
+  void jank::nested_group::test<0>()
+  { jank::common::translate("parse/comment/nested/pass_single_line.jank"); }
+
+  template <> template <>
+  void jank::nested_group::test<1>()
+  { jank::common::translate("parse/comment/nested/pass_single_line_multi_start.jank"); }
+
+  template <> template <>
+  void jank::nested_group::test<2>()
+  { jank::common::translate("parse/comment/nested/pass_single_line_multi_end.jank"); }
+
+  template <> template <>
+  void jank::nested_group::test<3>()
+  { jank::common::translate("parse/comment/nested/pass_multi_line.jank"); }
+
+  template <> template <>
+  void jank::nested_group::test<4>()
+  { jank::common::translate("parse/comment/nested/pass_multi_line_multi_start.jank"); }
+
+  template <> template <>
+  void jank::nested_group::test<5>()
+  { jank::common::translate("parse/comment/nested/pass_multi_line_multi_end.jank"); }
+
+  template <> template <>
+  void jank::nested_group::test<6>()
+  {
+    expect_exception<jank::parse::expect::error::syntax::exception<>>
+    ([]{ jank::common::translate("parse/comment/nested/fail_no_close.jank"); });
+  }
+}
--- test/src/cpp/parse/main.cpp
+++ test/src/cpp/parse/main.cpp
@@ -4,14 +4,13 @@
 #include "parse/string/escape.hpp"
 #include "parse/comment/single_line.hpp"
 #include "parse/comment/multi_line.hpp"
-//#include "parse/comment/nested.hpp"
+#include "parse/comment/nested.hpp"
+
 /* TODO:
- *  comments!
- *
  *  numbers,
  *    integers, reals (without tenths place), negative
  *  strings,
- *    escape, utf8
+ *    utf8
  *  etc.
  */
 
