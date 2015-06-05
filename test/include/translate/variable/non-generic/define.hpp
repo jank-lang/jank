3230215950d41f9dd9fcf02e990da731cf696203
jeaye <contact@jeaye.com>
2015-06-05 22:36:32 +0800

    Add tests for defining variables with functions

--- test/include/translate/variable/non-generic/define.hpp
+++ test/include/translate/variable/non-generic/define.hpp
@@ -27,43 +27,54 @@ namespace jest
 
   template <> template <>
   void jank::define_group::test<3>()
+  { jank::common::translate("translate/variable/non-generic/define/pass_builtin_function.jank"); }
+
+  template <> template <>
+  void jank::define_group::test<4>()
   {
     expect_exception<jank::translate::expect::error::type::exception<>>
     ([]{ jank::common::translate("translate/variable/non-generic/define/fail_unknown_type.jank"); });
   }
 
   template <> template <>
-  void jank::define_group::test<4>()
+  void jank::define_group::test<5>()
   {
     expect_exception<jank::translate::expect::error::syntax::exception<>>
     ([]{ jank::common::translate("translate/variable/non-generic/define/fail_missing_value.jank"); });
   }
 
   template <> template <>
-  void jank::define_group::test<5>()
+  void jank::define_group::test<6>()
   {
     expect_exception<jank::translate::expect::error::syntax::exception<>>
     ([]{ jank::common::translate("translate/variable/non-generic/define/fail_missing_type.jank"); });
   }
 
   template <> template <>
-  void jank::define_group::test<6>()
+  void jank::define_group::test<7>()
   {
     expect_exception<jank::translate::expect::error::type::exception<>>
     ([]{ jank::common::translate("translate/variable/non-generic/define/fail_incompatible_value.jank"); });
   }
 
   template <> template <>
-  void jank::define_group::test<7>()
+  void jank::define_group::test<8>()
   {
     expect_exception<jank::translate::expect::error::type::exception<>>
     ([]{ jank::common::translate("translate/variable/non-generic/define/fail_unknown_value_identifier.jank"); });
   }
 
   template <> template <>
-  void jank::define_group::test<8>()
+  void jank::define_group::test<9>()
   {
     expect_exception<jank::translate::expect::error::type::exception<>>
     ([]{ jank::common::translate("translate/variable/non-generic/define/fail_identifier_with_incompatible_type.jank"); });
+  }
+
+  template <> template <>
+  void jank::define_group::test<10>()
+  {
+    expect_exception<jank::translate::expect::error::type::exception<>>
+    ([]{ jank::common::translate("translate/variable/non-generic/define/fail_function_with_incompatible_type.jank"); });
   }
 }
