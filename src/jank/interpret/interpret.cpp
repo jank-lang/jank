fb201f3ec3b318a4f2ae8de0e903890060c1a02f
jeaye <contact@jeaye.com>
2015-06-22 22:20:26 +0800

    Fix some formatting

--- src/jank/interpret/interpret.cpp
+++ src/jank/interpret/interpret.cpp
@@ -54,7 +54,11 @@ namespace jank
       }
     }
 
-    parse::cell::cell interpret(std::shared_ptr<scope> const &env, translate::cell::function_body const &root)
+    parse::cell::cell interpret
+    (
+      std::shared_ptr<scope> const &env,
+      translate::cell::function_body const &root
+    )
     {
       static_cast<void>(env);
       for(auto const &c : root.data.cells)
@@ -63,7 +67,8 @@ namespace jank
         {
           case translate::cell::type::function_call:
           {
-            auto const &cell(translate::expect::type<translate::cell::type::function_call>(c));
+            auto const &cell
+            (translate::expect::type<translate::cell::type::function_call>(c));
 
             auto const next_scope(std::make_shared<scope>());
             next_scope->parent = env;
@@ -73,7 +78,8 @@ namespace jank
             {
               auto const &name(*arg_name_it++);
               auto const var(resolve_value(next_scope, arg.cell));
-              std::cout << "adding " << name.name << " to next scope as: " << var << std::endl;
+              std::cout << "adding " << name.name
+                        << " to next scope as: " << var << std::endl;
               next_scope->variables[name.name] = var;
             }
 
@@ -87,7 +93,8 @@ namespace jank
 
           case translate::cell::type::return_statement:
           {
-            auto const &cell(translate::expect::type<translate::cell::type::return_statement>(c));
+            auto const &cell
+            (translate::expect::type<translate::cell::type::return_statement>(c));
             std::cout << "returning: " << cell.data.cell << std::endl;
             return resolve_value(env, cell.data.cell);
           } break;
