8b2d8078b6a4719f4be95d7e624ed740751d356d
jeaye <contact@jeaye.com>
2015-06-22 22:27:47 +0800

    Add an internal exception to interpret

---
+++ include/jank/interpret/expect/error/internal/internal.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+namespace jank
+{
+  namespace interpret
+  {
+    namespace expect
+    {
+      namespace error
+      {
+        namespace internal
+        {
+          namespace detail
+          {
+            struct generic
+            { static char const constexpr *description{ "expression" }; };
+          }
+
+          template <typename T = detail::generic>
+          struct exception : std::runtime_error
+          {
+            exception()
+              : std::runtime_error
+                { std::string{ "internal interpret error (" } + T::description + ")" }
+            { }
+            exception(std::string const &s)
+              : std::runtime_error
+                { std::string{ "internal interpret error (" } + T::description + "): " + s }
+            { }
+          };
+        }
+      }
+    }
+  }
+}
---
+++ include/jank/interpret/expect/error/internal/unimplemented.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <jank/interpret/expect/error/internal/internal.hpp>
+
+namespace jank
+{
+  namespace interpret
+  {
+    namespace expect
+    {
+      namespace error
+      {
+        namespace internal
+        {
+          namespace detail
+          {
+            struct unimplemented
+            { static char const constexpr *description{ "unimplemented" }; };
+          }
+          using unimplemented = exception<detail::unimplemented>;
+        }
+      }
+    }
+  }
+}
