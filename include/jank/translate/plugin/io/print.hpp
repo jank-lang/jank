3f06ba380cd97a9a4560f4701c741df95f3d25f5
jeaye <contact@jeaye.com>
2015-06-30 21:12:12 +0800

    Try adding a unary print native definition

--- include/jank/translate/plugin/io/print.hpp
+++ include/jank/translate/plugin/io/print.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <jank/translate/environment/scope.hpp>
+#include <jank/translate/environment/builtin/type/primitive.hpp>
 
 namespace jank
 {
@@ -13,8 +14,19 @@ namespace jank
         /* TODO: move to cpp. */
         inline void print(std::shared_ptr<environment::scope> const &scope)
         {
-          static_cast<void>(scope);
-          /* TODO: DSL for specifying things? */
+          auto const nested_scope(std::make_shared<environment::scope>());
+          nested_scope->parent = scope;
+
+          cell::native_function_definition def
+          {
+            {
+              "print",
+              { { "i", environment::builtin::type::integer(scope) } },
+              environment::builtin::type::null(scope),
+              nested_scope
+            }
+          };
+          scope->native_function_definitions[def.data.name].emplace_back(std::move(def));
         }
       }
     }
