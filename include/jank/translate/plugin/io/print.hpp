958225359e0367104e384a00b665603edce680fc
jeaye <contact@jeaye.com>
2015-07-05 00:02:11 +0800

    Refact the native print plugin to a cpp

--- include/jank/translate/plugin/io/print.hpp
+++ include/jank/translate/plugin/io/print.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <jank/translate/environment/scope.hpp>
-#include <jank/translate/environment/builtin/type/primitive.hpp>
 
 namespace jank
 {
@@ -11,23 +10,7 @@ namespace jank
     {
       namespace io
       {
-        /* TODO: move to cpp. */
-        inline void print(std::shared_ptr<environment::scope> const &scope)
-        {
-          auto const nested_scope(std::make_shared<environment::scope>());
-          nested_scope->parent = scope;
-
-          cell::native_function_definition def
-          {
-            {
-              "print",
-              { { "i", environment::builtin::type::integer(scope) } },
-              environment::builtin::type::null(scope),
-              nested_scope
-            }
-          };
-          scope->native_function_definitions[def.data.name].emplace_back(std::move(def));
-        }
+        void print(std::shared_ptr<environment::scope> const &scope);
       }
     }
   }
---
+++ src/jank/translate/plugin/io/print.cpp
@@ -0,0 +1,32 @@
+#include <jank/translate/plugin/io/print.hpp>
+#include <jank/translate/environment/builtin/type/primitive.hpp>
+
+namespace jank
+{
+  namespace translate
+  {
+    namespace plugin
+    {
+      namespace io
+      {
+        void print(std::shared_ptr<environment::scope> const &scope)
+        {
+          auto const nested_scope(std::make_shared<environment::scope>());
+          nested_scope->parent = scope;
+
+          cell::native_function_definition def
+          {
+            {
+              "print",
+              { { "i", environment::builtin::type::integer(scope) } },
+              environment::builtin::type::null(scope),
+              nested_scope
+            }
+          };
+          scope->native_function_definitions[def.data.name].emplace_back
+          (std::move(def));
+        }
+      }
+    }
+  }
+}
