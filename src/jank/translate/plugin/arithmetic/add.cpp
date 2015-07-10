13e0a8433e0d5cac978e1c3de0b5ccf9980aec82
jeaye <contact@jeaye.com>
2015-07-10 21:12:46 +0800

    Add a native + for reals and strings

--- src/jank/translate/plugin/arithmetic/add.cpp
+++ src/jank/translate/plugin/arithmetic/add.cpp
@@ -12,11 +12,13 @@ namespace jank
     {
       namespace arithmetic
       {
+        template <typename F>
         static void make_binary_operator
         (
           std::shared_ptr<environment::scope> const &scope,
           std::string const &op,
-          cell::detail::type_reference const &type
+          cell::detail::type_reference const &type,
+          F const &apply
         )
         {
           auto const nested_scope(std::make_shared<environment::scope>());
@@ -28,23 +30,18 @@ namespace jank
               op,
               { { "data1", type }, { "data2", type } },
               type,
-              [](auto const &scope, auto const &args) -> cell::cell
+              [apply](auto const &scope, auto const &args) -> cell::cell
               {
                 if(args.size() != 2)
                 { throw expect::error::type::exception<>{ "invalid binary operator args" }; }
-
-                parse::cell::integer::type ret{};
-                for(auto const &arg : args)
-                {
-                  ret += parse::expect::type<parse::cell::type::integer>
-                  (interpret::resolve_value(scope, arg.cell)).data;
-                }
-                return environment::builtin::value::integer(ret);
+                return apply(scope, args);
               },
               nested_scope
             }
           };
 
+          /* TODO: It's easy to overwrite something here; provide an API to register
+           * native functions. */
           scope->native_function_definitions[def.data.name].emplace_back
           (std::move(def));
         }
@@ -53,7 +50,47 @@ namespace jank
         {
           /* TODO: Add op+ and * and / and - for all primitives. */
           make_binary_operator
-          (scope, "+", environment::builtin::type::integer(*scope));
+          (
+            scope, "+", environment::builtin::type::integer(*scope),
+            [](auto const &scope, auto const &args)
+            {
+              parse::cell::integer::type ret{};
+              for(auto const &arg : args)
+              {
+                ret += parse::expect::type<parse::cell::type::integer>
+                (interpret::resolve_value(scope, arg.cell)).data;
+              }
+              return environment::builtin::value::integer(ret);
+            }
+          );
+          make_binary_operator
+          (
+            scope, "+", environment::builtin::type::real(*scope),
+            [](auto const &scope, auto const &args)
+            {
+              parse::cell::real::type ret{};
+              for(auto const &arg : args)
+              {
+                ret += parse::expect::type<parse::cell::type::real>
+                (interpret::resolve_value(scope, arg.cell)).data;
+              }
+              return environment::builtin::value::real(ret);
+            }
+          );
+          make_binary_operator
+          (
+            scope, "+", environment::builtin::type::string(*scope),
+            [](auto const &scope, auto const &args)
+            {
+              parse::cell::string::type ret{};
+              for(auto const &arg : args)
+              {
+                ret += parse::expect::type<parse::cell::type::string>
+                (interpret::resolve_value(scope, arg.cell)).data;
+              }
+              return environment::builtin::value::string(ret);
+            }
+          );
         }
       }
     }
