9e0cd2bea17f294401e11475a3b914df6c4f2079
jeaye <contact@jeaye.com>
2015-07-10 21:15:02 +0800

    Refactor make_binary_operator out

--- src/jank/translate/plugin/arithmetic/add.cpp
+++ src/jank/translate/plugin/arithmetic/add.cpp
@@ -1,7 +1,6 @@
 #include <jank/parse/expect/type.hpp>
 #include <jank/translate/plugin/arithmetic/add.hpp>
-#include <jank/translate/environment/builtin/type/primitive.hpp>
-#include <jank/translate/environment/builtin/value/primitive.hpp>
+#include <jank/translate/plugin/arithmetic/make_binary_operator.hpp>
 #include <jank/interpret/environment/resolve_value.hpp>
 
 namespace jank
@@ -12,40 +11,6 @@ namespace jank
     {
       namespace arithmetic
       {
-        template <typename F>
-        static void make_binary_operator
-        (
-          std::shared_ptr<environment::scope> const &scope,
-          std::string const &op,
-          cell::detail::type_reference const &type,
-          F const &apply
-        )
-        {
-          auto const nested_scope(std::make_shared<environment::scope>());
-          nested_scope->parent = scope;
-
-          cell::native_function_definition def
-          {
-            {
-              op,
-              { { "data1", type }, { "data2", type } },
-              type,
-              [apply](auto const &scope, auto const &args) -> cell::cell
-              {
-                if(args.size() != 2)
-                { throw expect::error::type::exception<>{ "invalid binary operator args" }; }
-                return apply(scope, args);
-              },
-              nested_scope
-            }
-          };
-
-          /* TODO: It's easy to overwrite something here; provide an API to register
-           * native functions. */
-          scope->native_function_definitions[def.data.name].emplace_back
-          (std::move(def));
-        }
-
         void add(std::shared_ptr<environment::scope> const &scope)
         {
           /* TODO: Add op+ and * and / and - for all primitives. */
