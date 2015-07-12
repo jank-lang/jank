57c5f708e53d8163a304f1a6077fd5d111fe6653
jeaye <contact@jeaye.com>
2015-07-12 22:15:01 +0800

    Rename some jest groups

--- test/include/translate/plugin/assertion/assertion.hpp
+++ test/include/translate/plugin/assertion/assertion.hpp
@@ -6,20 +6,20 @@
 
 namespace jank
 {
-  struct call_test
+  struct assertion_test
   { };
-  using call_group = jest::group<call_test>;
-  static call_group const call_obj{ "assertions" };
+  using assertion_group = jest::group<assertion_test>;
+  static assertion_group const assertion_obj{ "assertions" };
 }
 
 namespace jest
 {
   template <> template <>
-  void jank::call_group::test<0>()
+  void jank::assertion_group::test<0>()
   { jank::common::interpret("translate/plugin/assertion/pass_true.jank"); }
 
   template <> template <>
-  void jank::call_group::test<2>()
+  void jank::assertion_group::test<2>()
   {
     expect_exception<jank::translate::expect::error::assertion::exception<>>
     ([]{ jank::common::interpret("translate/plugin/assertion/fail_false.jank"); });
