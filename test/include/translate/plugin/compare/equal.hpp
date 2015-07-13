7983a1aac1d5c23a498b460b35c758c767866dcd
jeaye <contact@jeaye.com>
2015-07-13 19:54:15 +0800

    Reorganize some test sources

--- test/include/translate/plugin/compare/equal.hpp
+++ test/include/translate/plugin/compare/equal.hpp
@@ -16,13 +16,13 @@ namespace jest
 {
   template <> template <>
   void jank::equal_group::test<0>()
-  { jank::common::interpret("translate/plugin/compare/pass_integer.jank"); }
+  { jank::common::interpret("translate/plugin/compare/equal/pass_integer.jank"); }
 
   template <> template <>
   void jank::equal_group::test<1>()
-  { jank::common::interpret("translate/plugin/compare/pass_real.jank"); }
+  { jank::common::interpret("translate/plugin/compare/equal/pass_real.jank"); }
 
   template <> template <>
   void jank::equal_group::test<2>()
-  { jank::common::interpret("translate/plugin/compare/pass_string.jank"); }
+  { jank::common::interpret("translate/plugin/compare/equal/pass_string.jank"); }
 }
