abd3c435bd4f0700044e0b22ef157f028cd0081d
jeaye <contact@jeaye.com>
2015-07-25 20:02:32 +0800

    Add some if definition tests

--- test/include/translate/if/define.hpp
+++ test/include/translate/if/define.hpp
@@ -15,16 +15,45 @@ namespace jest
 {
   template <> template <>
   void jank::define_group::test<0>()
-  { jank::common::translate("translate/if/define/pass_empty.jank"); }
+  { jank::common::translate("translate/if/define/pass_without_else.jank"); }
 
-  // TODO
-  // without else
-  // with else
-  // without both
-  // with null condition
-  // with integer condition
-  // with bool condition
-  // with bool function condition
-  // with return
-  // with return and else return
+  template <> template <>
+  void jank::define_group::test<1>()
+  { jank::common::translate("translate/if/define/pass_with_else.jank"); }
+
+  template <> template <>
+  void jank::define_group::test<2>()
+  {
+    expect_exception<jank::translate::expect::error::syntax::exception<>>
+    ([]{ jank::common::translate("translate/if/define/fail_without_both.jank"); });
+  }
+
+  template <> template <>
+  void jank::define_group::test<3>()
+  {
+    expect_exception<jank::translate::expect::error::type::exception<>>
+    ([]{ jank::common::translate("translate/if/define/fail_null_condition.jank"); });
+  }
+
+  template <> template <>
+  void jank::define_group::test<4>()
+  {
+    expect_exception<jank::translate::expect::error::type::exception<>>
+    ([]{ jank::common::translate("translate/if/define/fail_integer_condition.jank"); });
+  }
+
+  template <> template <>
+  void jank::define_group::test<5>()
+  { jank::common::translate("translate/if/define/pass_with_return.jank"); }
+
+  template <> template <>
+  void jank::define_group::test<6>()
+  {
+    expect_exception<jank::translate::expect::error::type::exception<>>
+    ([]{ jank::common::translate("translate/if/define/fail_with_return_missing_outer.jank"); });
+  }
+
+  template <> template <>
+  void jank::define_group::test<7>()
+  { jank::common::translate("translate/if/define/pass_with_return_and_else.jank"); }
 }
