d2b3bcdb897f8892f10a1623e6ae97f036684c20
jeaye <contact@jeaye.com>
2015-08-03 19:10:02 +0800

    Add apply_expression for specials

---
+++ include/jank/translate/environment/special/apply_expression.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <map>
+#include <stdexcept>
+#include <experimental/optional>
+
+#include <jank/translate/environment/special/if_statement.hpp>
+#include <jank/translate/environment/special/do_statement.hpp>
+
+namespace jank
+{
+  namespace translate
+  {
+    namespace environment
+    {
+      namespace special
+      {
+        std::experimental::optional<cell::cell> apply_expression
+        (
+          parse::cell::list const &list,
+          cell::function_body const &translated
+        );
+      }
+    }
+  }
+}
