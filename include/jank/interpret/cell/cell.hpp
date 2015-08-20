b1d528946c2e65a6f0023ea985f669b2f86903d5
jeaye <contact@jeaye.com>
2015-08-20 20:28:18 +0800

    Add new initial interpret cells

---
+++ include/jank/interpret/cell/cell.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <sstream>
+#include <cstdint>
+
+#include <boost/variant.hpp>
+
+#include <jank/parse/cell/cell.hpp>
+#include <jank/interpret/cell/type.hpp>
+
+namespace jank
+{
+  namespace interpret
+  {
+    namespace cell
+    {
+      template <type C>
+      struct wrapper;
+
+      using cell = boost::variant
+      <
+        boost::recursive_wrapper<wrapper<type::null>>,
+        boost::recursive_wrapper<wrapper<type::boolean>>,
+        boost::recursive_wrapper<wrapper<type::integer>>,
+        boost::recursive_wrapper<wrapper<type::real>>,
+        boost::recursive_wrapper<wrapper<type::string>>,
+        boost::recursive_wrapper<wrapper<type::function>>
+      >;
+
+      template <>
+      struct wrapper<type::null>
+      {
+        using type = parse::cell::null::type;
+        type data;
+      };
+      template <>
+      struct wrapper<type::boolean>
+      {
+        using type = parse::cell::boolean::type;
+        type data;
+      };
+      template <>
+      struct wrapper<type::integer>
+      {
+        using type = parse::cell::integer::type;
+        type data;
+      };
+      template <>
+      struct wrapper<type::real>
+      {
+        using type = parse::cell::real::type;
+        type data;
+      };
+      template <>
+      struct wrapper<type::string>
+      {
+        using type = parse::cell::string::type;
+        type data;
+      };
+      template <>
+      struct wrapper<type::function>
+      {
+        /* TODO: Take arg list, scope, etc. */
+        using type = std::function<void ()>;
+        type data;
+      };
+
+      using null = wrapper<type::null>;
+      using boolean = wrapper<type::boolean>;
+      using integer = wrapper<type::integer>;
+      using real = wrapper<type::real>;
+      using string = wrapper<type::string>;
+      using function = wrapper<type::function>;
+    }
+  }
+}
---
+++ include/jank/interpret/cell/type.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace jank
+{
+  namespace interpret
+  {
+    namespace cell
+    {
+      enum class type
+      {
+        null,
+        boolean,
+        integer,
+        real,
+        string,
+        function
+      };
+    }
+  }
+}
