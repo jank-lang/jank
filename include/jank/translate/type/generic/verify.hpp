86d510e8badc324d0b684f3bfb4f0f12f9ebafc1
jeaye <contact@jeaye.com>
2015-08-24 23:34:56 +0800

    Stub out generic type verification

---
+++ include/jank/translate/type/generic/verify.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <jank/translate/cell/detail/type_definition.hpp>
+#include <jank/translate/type/generic/genericity.hpp>
+#include <jank/translate/expect/error/type/invalid_generic.hpp>
+
+namespace jank
+{
+  namespace translate
+  {
+    namespace type
+    {
+      namespace generic
+      {
+        inline void verify /* TODO: move to cpp */
+        (
+          genericity<cell::detail::type_definition> const &expected,
+          genericity<cell::detail::type_definition> const &provided
+        )
+        {
+          if(expected.parameters.size() != provided.parameters.size())
+          {
+            throw expect::error::type::invalid_generic
+            { "missing generic parameters" };
+          }
+
+          auto const equal
+          (
+            std::equal
+            (
+              expected.parameters.begin(), expected.parameters.end(),
+              provided.parameters.begin()
+            )
+          );
+          if(!equal)
+          {
+            throw expect::error::type::invalid_generic
+            { "incorrect generic parameters" };
+          }
+        }
+      }
+    }
+  }
+}
