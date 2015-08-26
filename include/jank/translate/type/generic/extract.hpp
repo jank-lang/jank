7793c03f3c66e7be7fc72700b253fecf03f21e9c
jeaye <contact@jeaye.com>
2015-08-26 21:56:20 +0800

    Stub out generic type extraction

---
+++ include/jank/translate/type/generic/extract.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <jank/parse/cell/cell.hpp>
+#include <jank/parse/expect/type.hpp>
+#include <jank/translate/expect/error/type/invalid_generic.hpp>
+
+namespace jank
+{
+  namespace translate
+  {
+    namespace type
+    {
+      namespace generic
+      {
+        /* Tries to find a generic specialization. */
+        inline std::tuple
+        <
+          std::experimental::optional<parse::cell::list>,
+          parse::cell::list::type::iterator
+        > extract /* TODO: move to cpp */
+        (
+          parse::cell::list::type::iterator begin,
+          parse::cell::list::type::iterator const end
+        )
+        {
+          auto const &colon
+          (parse::expect::optional_cast<parse::cell::type::ident>(*begin));
+          if(colon.value().data == ":")
+          {
+            if(++begin == end)
+            {
+              throw expect::error::type::invalid_generic
+              { "no type list after colon" };
+            }
+            return
+            {
+              std::experimental::optional<parse::cell::list>
+              { parse::expect::type<parse::cell::type::list>(*begin) },
+              std::next(begin)
+            };
+          }
+
+          return
+          {
+            std::experimental::optional<parse::cell::list>{},
+            std::next(begin)
+          };
+        }
+      }
+    }
+  }
+}
