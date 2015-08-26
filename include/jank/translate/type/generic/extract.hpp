f4c20f7ef724aa7c52f64a934067d5cb479d7397
jeaye <contact@jeaye.com>
2015-08-26 23:47:14 +0800

    Refactor generic extraction to a cpp

--- include/jank/translate/type/generic/extract.hpp
+++ include/jank/translate/type/generic/extract.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <experimental/optional>
+
 #include <jank/parse/cell/cell.hpp>
-#include <jank/parse/expect/type.hpp>
-#include <jank/translate/expect/error/type/invalid_generic.hpp>
 
 namespace jank
 {
@@ -13,44 +13,15 @@ namespace jank
       namespace generic
       {
         /* Tries to find a generic specialization. */
-        inline std::tuple
+        std::tuple
         <
           std::experimental::optional<parse::cell::list>,
           parse::cell::list::type::const_iterator
-        > extract /* TODO: move to cpp */
+        > extract
         (
           parse::cell::list::type::const_iterator const begin,
           parse::cell::list::type::const_iterator const end
-        )
-        {
-          auto const colon_it(std::next(begin));
-          auto const &colon
-          (
-            parse::expect::optional_cast<parse::cell::type::ident>
-            (*colon_it)
-          );
-          if(colon.value().data == ":")
-          {
-            auto const list_it(std::next(colon_it));
-            if(list_it == end)
-            {
-              throw expect::error::type::invalid_generic
-              { "no type list after colon" };
-            }
-            return std::make_tuple
-            (
-              std::experimental::optional<parse::cell::list>
-              { parse::expect::type<parse::cell::type::list>(*list_it) },
-              list_it
-            );
-          }
-
-          return std::make_tuple
-          (
-            std::experimental::optional<parse::cell::list>{},
-            begin
-          );
-        }
+        );
       }
     }
   }
---
+++ src/jank/translate/type/generic/extract.cpp
@@ -0,0 +1,54 @@
+#include <jank/parse/expect/type.hpp>
+#include <jank/translate/type/generic/extract.hpp>
+#include <jank/translate/expect/error/type/invalid_generic.hpp>
+
+namespace jank
+{
+  namespace translate
+  {
+    namespace type
+    {
+      namespace generic
+      {
+        std::tuple
+        <
+          std::experimental::optional<parse::cell::list>,
+          parse::cell::list::type::const_iterator
+        > extract
+        (
+          parse::cell::list::type::const_iterator const begin,
+          parse::cell::list::type::const_iterator const end
+        )
+        {
+          auto const colon_it(std::next(begin));
+          auto const &colon
+          (
+            parse::expect::optional_cast<parse::cell::type::ident>
+            (*colon_it)
+          );
+          if(colon.value().data == ":")
+          {
+            auto const list_it(std::next(colon_it));
+            if(list_it == end)
+            {
+              throw expect::error::type::invalid_generic
+              { "no type list after colon" };
+            }
+            return std::make_tuple
+            (
+              std::experimental::optional<parse::cell::list>
+              { parse::expect::type<parse::cell::type::list>(*list_it) },
+              list_it
+            );
+          }
+
+          return std::make_tuple
+          (
+            std::experimental::optional<parse::cell::list>{},
+            begin
+          );
+        }
+      }
+    }
+  }
+}
