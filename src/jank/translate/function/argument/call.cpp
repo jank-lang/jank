bce460640bf1ecef5369f281eb7b4ea999f8016f
jeaye <contact@jeaye.com>
2015-09-01 23:27:33 +0800

    Add a todo

--- src/jank/translate/function/argument/call.cpp
+++ src/jank/translate/function/argument/call.cpp
@@ -159,6 +159,7 @@ namespace jank
                   }
                 }
 
+                /* TODO: Indirect function calls/ */
                 auto const name
                 (parse::expect::type<parse::cell::type::ident>(c.data[0]).data);
                 auto const native_function_opt
