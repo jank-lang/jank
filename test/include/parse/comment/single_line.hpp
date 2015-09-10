6e7b060b1240ca9fa2a860cd57ab2c2a735e1981
jeaye <contact@jeaye.com>
2015-09-10 20:35:10 +0800

    Add a failing test for a comment bug

--- test/include/parse/comment/single_line.hpp
+++ test/include/parse/comment/single_line.hpp
@@ -37,6 +37,10 @@ namespace jest
 
   template <> template <>
   void jank::single_line_group::test<5>()
+  { jank::common::translate("parse/comment/single_line/pass_multiple_in_one_file.jank"); }
+
+  template <> template <>
+  void jank::single_line_group::test<6>()
   {
     expect_exception<jank::parse::expect::error::syntax::exception<>>
     ([]{ jank::common::translate("parse/comment/single_line/fail_no_close.jank"); });
