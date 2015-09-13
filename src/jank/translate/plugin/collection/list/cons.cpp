fc9947681cb06a250d5fa3bdf4be87c2b0b42dab
jeaye <contact@jeaye.com>
2015-09-13 21:42:32 +0800

    Add a sample native first plugin

--- src/jank/translate/plugin/collection/list/cons.cpp
+++ src/jank/translate/plugin/collection/list/cons.cpp
@@ -63,6 +63,27 @@ namespace jank
               elem_type, col_type
             );
 
+            plugin::detail::make_function
+            (
+              scope, "first",
+              [](auto const &scope, auto const &args)
+              {
+                auto coll
+                (
+                  interpret::expect::type<interpret::cell::type::list>
+                  (interpret::environment::resolve_value(scope, args[0].cell)).data
+                );
+                auto const i
+                (
+                  interpret::expect::type<interpret::cell::type::integer>
+                  (coll.front()).data
+                );
+                return cell::cell{ cell::literal_value{ parse::cell::integer{ i } } };
+              },
+              col_type,
+              col_type
+            );
+
             /* TODO: Remove */
             plugin::detail::make_function
             (
