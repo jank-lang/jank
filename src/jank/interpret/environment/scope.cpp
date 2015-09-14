eaa93e6be2913177ae88e71846990fd4d7906b94
jeaye <contact@jeaye.com>
2015-09-14 14:15:57 +0800

    Add scope's find_native_function

--- src/jank/interpret/environment/scope.cpp
+++ src/jank/interpret/environment/scope.cpp
@@ -19,6 +19,20 @@ namespace jank
         }
         return { it->second };
       }
+
+      std::experimental::optional<plugin::detail::native_function_definition>
+      scope::find_native_function(std::string const &name) const
+      {
+        auto const it(native_function_definitions.find(name));
+        if(it == native_function_definitions.end())
+        {
+          if(parent)
+          { return parent->find_native_function(name); }
+          else
+          { return {}; }
+        }
+        return { it->second };
+      }
     }
   }
 }
