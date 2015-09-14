2d88185c5ae646eca2a241a4173e29e4f8afa820
jeaye <contact@jeaye.com>
2015-09-14 14:09:49 +0800

    Add native function definitions to interpret scope

--- include/jank/interpret/environment/scope.hpp
+++ include/jank/interpret/environment/scope.hpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <experimental/optional>
 
-#include <jank/interpret/cell/cell.hpp>
+#include <jank/interpret/plugin/detail/native_function_definition.hpp>
 
 namespace jank
 {
@@ -22,8 +22,15 @@ namespace jank
 
         std::experimental::optional<cell::cell> find_binding
         (std::string const &name) const;
+        std::experimental::optional<plugin::detail::native_function_definition>
+        find_native_function(std::string const &name) const;
 
         std::map<std::string, cell::cell> bindings;
+        std::map
+        <
+          std::string,
+          plugin::detail::native_function_definition
+        > native_function_definitions;
         std::shared_ptr<scope> parent;
       };
     }
