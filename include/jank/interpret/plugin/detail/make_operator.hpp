be5cafadaea2ea962c8c8575ec34444ea985bad6
jeaye <contact@jeaye.com>
2015-09-19 22:16:15 +0800

    Add a make_operator for unary native definitions

--- include/jank/interpret/plugin/detail/make_operator.hpp
+++ include/jank/interpret/plugin/detail/make_operator.hpp
@@ -17,6 +17,9 @@ namespace jank
         template <cell::type T1, cell::type T2, cell::type Ret>
         struct binary_operator_wrapper
         { };
+        template <cell::type T1, cell::type Ret>
+        struct unary_operator_wrapper
+        { };
 
         /* Helper wrapper around make_function for binary ops. */
         template <typename F, cell::type T1, cell::type T2, cell::type Ret>
@@ -52,6 +55,35 @@ namespace jank
               return cell::cell{ cell::trait::to_type<Ret>{ apply(v1, v2) } };
             },
             type1, type2
+          );
+        }
+
+        template <typename F, cell::type T1, cell::type Ret>
+        void make_operator
+        (
+          std::shared_ptr<translate::environment::scope> const &trans_scope,
+          std::shared_ptr<environment::scope> const &int_scope,
+          std::string const &name,
+          translate::cell::detail::type_reference
+          <translate::cell::cell> const &type1,
+          unary_operator_wrapper<T1, Ret> const&,
+          F const &apply
+        )
+        {
+          make_function
+          (
+            trans_scope, int_scope,
+            name,
+            [apply](auto const &scope, auto const &args)
+            {
+              auto const v1
+              (
+                expect::type<T1>
+                (environment::resolve_value(scope, args[0].cell)).data
+              );
+              return cell::cell{ cell::trait::to_type<Ret>{ apply(v1) } };
+            },
+            type1
           );
         }
       }
