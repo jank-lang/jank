ba23a44860a8447ed1a02d50e33d00c1a2d8603c
jeaye <contact@jeaye.com>
2015-09-26 12:33:44 +0800

    Refactor consume_style out of interpret

--- include/jank/interpret/interpret.hpp
+++ include/jank/interpret/interpret.hpp
@@ -5,30 +5,13 @@
 
 #include <jank/translate/cell/cell.hpp>
 #include <jank/interpret/cell/cell.hpp>
+#include <jank/interpret/consume_style.hpp>
 #include <jank/interpret/environment/scope.hpp>
 
 namespace jank
 {
   namespace interpret
   {
-    /* TODO: Move to its own header? */
-    /* Says whether or not values from function calls should be returned.
-     * By default, any value that isn't from a return statement is ignored.
-     * This is used when interpreting a parameter to a function, since the value
-     * will be used. Furthermore, in the repl, all values are wanted. */
-    enum class consume_style
-    {
-      normal,
-      greedy,
-      all
-    };
-
-    inline bool operator >(consume_style const lhs, consume_style const rhs)
-    {
-      using underlying = std::underlying_type_t<consume_style>;
-      return static_cast<underlying>(lhs) > static_cast<underlying>(rhs);
-    }
-
     cell::cell interpret
     (
       std::shared_ptr<environment::scope> const &env,
