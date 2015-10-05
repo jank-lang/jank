0d51ff7c0fddd68aa9213941cdc8525148addc2d
jeaye <contact@jeaye.com>
2015-10-05 20:10:53 +0800

    Add macro primitive types ^atom and ^list

--- src/jank/translate/main.cpp
+++ src/jank/translate/main.cpp
@@ -17,6 +17,7 @@
 #include <jank/translate/translate.hpp>
 #include <jank/translate/cell/stream.hpp>
 #include <jank/translate/environment/builtin/type/primitive.hpp>
+#include <jank/translate/environment/builtin/type/macro_primitive.hpp>
 #include <jank/translate/environment/builtin/type/function.hpp>
 #include <jank/translate/environment/builtin/type/list.hpp>
 #include <jank/translate/plugin/apply.hpp>
@@ -70,6 +71,7 @@ int main(int const argc, char ** const argv)
   auto const scope
   (std::make_shared<jank::translate::environment::scope>(nullptr));
   jank::translate::environment::builtin::type::add_primitives(*scope);
+  jank::translate::environment::builtin::type::add_macro_primitives(*scope);
   jank::translate::environment::builtin::type::add_function(*scope);
   jank::translate::environment::builtin::type::add_list(*scope);
   jank::translate::plugin::apply(scope);
