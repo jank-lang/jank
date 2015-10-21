aa924a2c84f2f8059ef9a373751fd3652455c9e6
jeaye <contact@jeaye.com>
2015-10-21 21:51:42 +0800

    Stub out some codegen work

--- jank/src/jank/core.clj
+++ jank/src/jank/core.clj
@@ -34,16 +34,37 @@
 (defmethod handle :default [current ast]
   current)
 
-(defn empty-ast []
-  {:cells []})
+(defmulti codegen-impl
+  (fn [current]
+    (first current)))
+
+(defmethod codegen-impl :lambda-definition [current]
+  "")
+
+(defmethod codegen-impl :macro-definition [current]
+  "")
+
+(defmethod codegen-impl :binding-definition [current]
+  "")
+
+(defmethod codegen-impl :function-call [current]
+  "")
+
+(defmethod codegen-impl :default [current]
+  "")
+
+(defn codegen [ast]
+  (doseq [current (:cells ast)]
+    (println current)))
 
 (defn -main
   [& args]
-  (println
+  (codegen
     (let [parsed (parse (slurp (first args)))]
       (when parsed
-        (loop [current (first parsed) remaining (rest parsed)
-               ast (empty-ast)]
+        (loop [current (first parsed)
+               remaining (rest parsed)
+               ast {:cells []}]
           (println current)
           (cond
             (nil? current) ast
