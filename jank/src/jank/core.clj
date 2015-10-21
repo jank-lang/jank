11d0e5a78b3eb7e46ece0a75b0cc4085f84240fd
jeaye <contact@jeaye.com>
2015-10-21 21:23:36 +0800

    Add map-after

--- jank/src/jank/core.clj
+++ jank/src/jank/core.clj
@@ -7,12 +7,16 @@
     (clojure.java.io/resource "grammar")
     :auto-whitespace :standard))
 
+(defn map-after [index func coll]
+  (vec (concat (take index coll)
+               (map func (drop index coll)))))
+
 (defmulti handle
   (fn [current ast]
     (first current)))
 
 (defmethod handle :lambda-definition [current ast]
-  current)
+  (map-after 3 #(handle %1 ast) current))
 
 (defmethod handle :macro-definition [current ast]
   current)
@@ -31,14 +35,14 @@
 
 (defn -main
   [& args]
-  (let [parsed (parse (slurp (first args)))]
-    (when parsed
-      (loop [current (first parsed) remaining (rest parsed)
-             ast (empty-ast)]
-        (println current)
-        (cond
-          (nil? current) ast
-          :else (recur (first remaining)
-                       (rest remaining)
-                       (update ast :cells conj (handle current ast)))
-        )))))
+  (println
+    (let [parsed (parse (slurp (first args)))]
+      (when parsed
+        (loop [current (first parsed) remaining (rest parsed)
+               ast (empty-ast)]
+          (println current)
+          (cond
+            (nil? current) ast
+            :else (recur (first remaining)
+                         (rest remaining)
+                         (update ast :cells conj (handle current ast)))))))))
