caeb91538c38b3fe557fc6a67d751d0575c51505
jeaye <contact@jeaye.com>
2015-10-21 11:49:11 +0800

    Start looping through the syntax tree

--- jank/src/jank/core.clj
+++ jank/src/jank/core.clj
@@ -2,12 +2,25 @@
   (:gen-class)
   (:require [instaparse.core :as insta]))
 
-(def grammar
+(def parse
   (insta/parser
     (clojure.java.io/resource "grammar")
     :auto-whitespace :standard))
 
+(defn handle [current ast]
+  (println current)
+  ast
+  )
+
 (defn -main
   [& args]
-  (println
-    (grammar (slurp (first args)))))
+  (let [parsed (parse (slurp (first args)))]
+    (when parsed
+      (loop [current (first parsed) remaining (rest parsed)
+             ast '[]]
+        (cond
+          (nil? current) ast
+          :else (recur (first remaining)
+                       (rest remaining)
+                       (handle current ast))
+        )))))
