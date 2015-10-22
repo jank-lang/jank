8fb78e45016e6b0ce0068b8bff64ddab4a31b45e
jeaye <contact@jeaye.com>
2015-10-22 16:07:12 +0800

    Start adding codegen for lambdas

--- jank/src/jank/core.clj
+++ jank/src/jank/core.clj
@@ -34,12 +34,25 @@
 (defmethod handle :default [current ast]
   current)
 
+(defn swap-args [args]
+  "Takes the input (i integer b boolean) and gives the C-like
+   representation: ((integer i) (boolean b))"
+  (map reverse (partition 2 args)))
+
+(defn comma-separate [pairs]
+  "Turns ((integer i) (boolean b)) into a string like
+   \"integer i, boolean b\""
+  (clojure.string/join ", "
+                       (map #(str (first %1) " " (second %1))
+                            pairs)))
 (defmulti codegen-impl
   (fn [current]
     (first current)))
 
 (defmethod codegen-impl :lambda-definition [current]
-  "")
+  (str "[&]"
+       (codegen-impl (second current)) ; Args
+       ))
 
 (defmethod codegen-impl :macro-definition [current]
   "")
@@ -50,12 +63,22 @@
 (defmethod codegen-impl :function-call [current]
   "")
 
+(defmethod codegen-impl :argument-list [current]
+  (str "("
+       (comma-separate
+         (swap-args
+           (map second (rest (second current)))))
+       ")"))
+
+(defmethod codegen-impl :list [current]
+  "")
+
 (defmethod codegen-impl :default [current]
   "")
 
 (defn codegen [ast]
   (doseq [current (:cells ast)]
-    (println current)))
+    (println (codegen-impl current))))
 
 (defn -main
   [& args]
