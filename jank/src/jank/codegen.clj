4511234b07b1dbc8c298e9446c39ebad816a1cf5
jeaye <contact@jeaye.com>
2015-10-25 17:50:03 +0800

    Add codegen for do statements

--- jank/src/jank/codegen.clj
+++ jank/src/jank/codegen.clj
@@ -135,6 +135,12 @@
                                  "}")
       :else base)))
 
+(defmethod codegen-impl :do-statement [current]
+  (str "[&]{"
+       (reduce-spaced-map (comp end-statement codegen-impl)
+                          (drop 1 current))
+       "}"))
+
 (defmethod codegen-impl :list [current]
   (str "("
        (reduce-spaced-map codegen-impl (rest current))
