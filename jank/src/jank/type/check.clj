aef2f5614acd184ae173ec868db72532fbff46b8
jeaye <contact@jeaye.com>
2015-11-01 20:06:18 +0800

    Build up the type checked tree

--- jank/src/jank/type/check.clj
+++ jank/src/jank/type/check.clj
@@ -1,4 +1,14 @@
 (ns jank.type.check)
 
 (defn check [parsed]
-  parsed)
+  (loop [item (first (:cells parsed))
+         remaining (rest (:cells parsed))
+         checked []]
+    (cond
+      (empty? remaining)
+      (conj checked item)
+      :else
+      (do
+        (recur (first remaining)
+               (rest remaining)
+               (conj checked item))))))
