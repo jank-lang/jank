4a9552d0fe17112c0993dbb1f20c8cb3c4f933ba
jeaye <contact@jeaye.com>
2015-11-03 15:36:45 +0800

    Stub out multi-dispatch for type checking

--- jank/src/jank/type/check.clj
+++ jank/src/jank/type/check.clj
@@ -1,12 +1,55 @@
 (ns jank.type.check)
 
+(defmulti check-item
+  (fn [item]
+    (first item)))
+
+(defmethod check-item :function-definition [item]
+  item)
+
+(defmethod check-item :lambda-definition [item]
+  item)
+
+(defmethod check-item :binding-definition [item]
+  item)
+
+(defmethod check-item :function-call [item]
+  item)
+
+(defmethod check-item :argument-list [item]
+  item)
+
+(defmethod check-item :if-statement [item]
+  item)
+
+(defmethod check-item :list [item]
+  item)
+
+(defmethod check-item :string [item]
+  item)
+
+(defmethod check-item :integer [item]
+  item)
+
+(defmethod check-item :real [item]
+  item)
+
+(defmethod check-item :boolean [item]
+  item)
+
+(defmethod check-item :identifier [item]
+  item)
+
+(defmethod check-item :default [item]
+  (assert false (str "no type checking for '" item "'")))
+
 ; TODO
 ; Add :type
 ; Add :scope {:parent {}
 ;             :bindings []}
 (defn check [parsed]
   "Builds type information on the parsed source. Returns
-   the typed source and the top-level scope."
+   a cons of the typed source and the top-level scope."
   (loop [item (first (:cells parsed))
          remaining (rest (:cells parsed))
          checked []
@@ -17,5 +60,5 @@
       :else
       (recur (first remaining)
              (rest remaining)
-             (conj checked item)
+             (conj checked (check-item item))
              scope))))
