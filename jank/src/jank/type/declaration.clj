77cf01ff15b46302ca4c7a74f478591d9fefa5c0
jeaye <contact@jeaye.com>
2015-11-04 14:06:34 +0800

    Clean up some type checking

--- jank/src/jank/type/declaration.clj
+++ jank/src/jank/type/declaration.clj
@@ -14,14 +14,15 @@
    declaration has a matching type. Returns the decl or nil, if none is found."
   (let [decl (lookup decl-name scope)]
     (when (some? decl)
-      ; TODO: Allow overloads
-      (assert (= (:type (second decl)) decl-type)
-              (str "Declaration of "
-                   decl-name
-                   " doesn't match previous declarations: "
-                   (:type (second decl))
-                   " vs "
-                   decl-type)))
+      (let [expected-type (:type (second decl))]
+        ; TODO: Allow overloads
+        (assert (= expected-type decl-type)
+                (str "Declaration of "
+                     decl-name
+                     " doesn't match previous declarations: "
+                     expected-type
+                     " vs "
+                     decl-type))))
     decl))
 
 (defn add-to-scope [item scope]
