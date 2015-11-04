7439a6bbb0322cb0dc7628129dd857c9036a0c90
jeaye <contact@jeaye.com>
2015-11-04 13:54:25 +0800

    Refactor out declaration type code

--- jank/src/jank/type/check.clj
+++ jank/src/jank/type/check.clj
@@ -1,39 +1,5 @@
-(ns jank.type.check)
-
-(defn lookup-declaration [decl-name scope]
-  "Recursively looks through the hierarchy of scopes for the declaration."
-  (loop [current-scope scope]
-    (when current-scope
-      ; TODO: Should be a vector of overloads
-      (if-let [found (find (:declarations current-scope) decl-name)]
-        found
-        (recur (:parent current-scope))))))
-
-(defn validate-declaration [decl-name decl-type scope]
-  "Looks up a declaration, if any, and verifies that the provided
-   declaration has a matching type. Returns the decl or nil, if none is found."
-  (let [decl (lookup-declaration decl-name scope)]
-    (when (some? decl)
-      (assert (= (:type (second decl)) decl-type)
-              (str "Declaration of "
-                   decl-name
-                   " doesn't match previous declarations: "
-                   (:type (second decl))
-                   " vs "
-                   decl-type)))
-    decl))
-
-(defn add-declaration [item scope]
-  "Finds, validates, and adds the provided declaration into the scope.
-   Returns the updated scope."
-  (let [decl-name (get-in item [1 1])
-        decl-type (get-in item [2 1])
-        decl (validate-declaration decl-name decl-type scope)]
-    (cond
-      (nil? decl)
-      (update scope :declarations assoc decl-name {:type decl-type})
-      :else
-      scope)))
+(ns jank.type.check
+  (:require [jank.type.declaration :as declaration :refer [add-to-scope]]))
 
 (defmulti check-item
   "Type checks the given expression. Returns a cons of the typed
@@ -43,7 +9,7 @@
 
 (defmethod check-item :declare-statement [item scope]
   ; TODO: Add type information to the item
-  (list item (add-declaration item scope)))
+  (list item (declaration/add-to-scope item scope)))
 
 (defmethod check-item :function-definition [item scope]
   (list item scope))
