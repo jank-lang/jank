37970dfa050f6fbc21f65e6b933e42a21abfb48f
jeaye <contact@jeaye.com>
2015-11-05 14:28:55 +0800

    Add type checking for type declarations

--- jank/src/jank/type/declaration.clj
+++ jank/src/jank/type/declaration.clj
@@ -5,7 +5,7 @@
   (loop [current-scope scope]
     (when current-scope
       ; TODO: Should be a vector of overloads
-      (if-let [found (find (:declarations current-scope) decl-name)]
+      (if-let [found (find (:binding-declarations current-scope) decl-name)]
         found
         (recur (:parent current-scope))))))
 
@@ -25,12 +25,24 @@
                      decl-type))))
     decl))
 
-(defn add-to-scope [item scope]
+(defmulti add-to-scope
+  (fn [item scope]
+    (if (= (count item) 2)
+      :type-declaration
+      :binding-declaration)))
+
+(defmethod add-to-scope :type-declaration [item scope]
+  "Adds the opaque type declaration to the scope.
+   Returns the updated scope."
+  (let [decl-name (get-in item [1 1])]
+    (update scope :type-declarations conj decl-name)))
+
+(defmethod add-to-scope :binding-declaration [item scope]
   "Finds, validates, and adds the provided declaration into the scope.
    Returns the updated scope."
   (let [decl-name (get-in item [1 1])
         decl-type (get-in item [2 1])
         decl (validate decl-name decl-type scope)]
     (if (nil? decl)
-      (update scope :declarations assoc decl-name {:type decl-type})
+      (update scope :binding-declarations assoc decl-name {:type decl-type})
       scope)))
