23d82d04909776911fa18076df20d814f771cfa0
jeaye <contact@jeaye.com>
2015-11-14 16:55:54 +0800

    Move if expression branches into nested scopes

--- jank/src/jank/type/check.clj
+++ jank/src/jank/type/check.clj
@@ -104,12 +104,12 @@
     (assert (= cond-type '("boolean"))
             (str "if statement condition must be boolean, not: " cond-type))
     (let [[checked-then then-scope] (check {:cells (rest (get-in item [2]))}
-                                           scope)
+                                           (empty-scope scope))
           updated-item (update-in item [2]
                                   (fn [_] (into [:then] (:cells checked-then))))]
       (if (> (count item) 3) ; There's an else
         (let [[checked-else else-scope] (check {:cells (rest (get-in item [3]))}
-                                               scope)]
+                                               (empty-scope scope))]
           (list (update-in updated-item [3]
                            (fn [_] (into [:else] (:cells checked-else)))) scope))
         (list updated-item scope)))))
