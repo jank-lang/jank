9da2da58388c8429c895c440616b46d276072dc2
jeaye <contact@jeaye.com>
2015-11-15 20:32:45 +0800

    Update project.clj info

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject jank "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A statically typed functional programming language"
+  :url "https://github.com/jeaye/jank"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [instaparse "1.4.1"]]
   :main ^:skip-aot jank.core
