45afbf3fbace32631b783dc4c2d3c814a7450a61
jeaye <contact@jeaye.com>
2015-11-15 20:23:55 +0800

    Have travis build the clojure implementation

--- travis.sh
+++ travis.sh
@@ -1,32 +1,7 @@
 #/bin/bash
 
-exit 0
 set -e
 
-if [[ "$analyze" == "true" ]]; then
-  scan_build="scan-build --use-cc=$(which $CC) --use-c++ --use-c++=$(which $CXX)"
-  analyze_suffix=_analyze
+lein uberjar
 
-  if [[ "$CXX" == "g++" ]]; then
-    # No need to analyze for both gcc and clang
-    exit 0
-  fi
-else
-  scan_build=
-  analyze_suffix=
-fi
-
-# Release build
-dir=build_release$analyze_suffix
-mkdir $dir ; cd $dir
-$scan_build cmake .. -DCMAKE_BUILD_TYPE=RELEASE
-$scan_build make -j2
-$scan_build make test
-cd ..
-
-# Debug build
-dir=build_debug$analyze_suffix
-mkdir $dir ; cd $dir
-$scan_build cmake .. -DCMAKE_BUILD_TYPE=DEBUG
-$scan_build make -j2
-$scan_build make test
+# lein test
