8d00c99d63654ec993614191f9fe336ee783ad3c
jeaye <contact@jeaye.com>
2015-11-16 13:50:54 +0800

    Fix up initial parse testing

--- src/jank/parse.clj
+++ src/jank/parse.clj
@@ -1,7 +1,15 @@
 (ns jank.parse
   (:require [instaparse.core :as insta]))
 
-(def parse
+(def parser
   (insta/parser
     (clojure.java.io/resource "grammar")
     :auto-whitespace :standard))
+
+(defn parse [resource]
+  "Runs the provided resource file through instaparse. Returns
+   then generated syntax tree."
+  (let [parsed (parser resource)]
+    (assert (not (insta/failure? parsed))
+            (str "failed to parse"))
+    parsed))
--- test/jank/parse_test.clj
+++ test/jank/parse_test.clj
@@ -3,5 +3,5 @@
             [jank.bootstrap :refer :all]))
 
 (deftest parse
-  (is (valid-parse? "test/parse/paren/match/fail_close_nothing_else.jank"))
-  (is (valid-parse? "test/parse/ident/ascii/pass_good_chars.jank")))
+  (is (thrown? AssertionError (valid-type? "test.jank")))
+  )
