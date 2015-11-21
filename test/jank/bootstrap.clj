461550ed1a0c73d4dd35b7ff8caac103ff0383aa
jeaye <contact@jeaye.com>
2015-11-21 15:11:52 +0800

    Clean up type test output

--- test/jank/bootstrap.clj
+++ test/jank/bootstrap.clj
@@ -4,11 +4,18 @@
             [jank.type.check :as type]
             [jank.codegen :as codegen]))
 
+(defmacro consume-output
+  [& body]
+  `(let [s# (new java.io.StringWriter)]
+     (binding [*out* s# *err* s#]
+       ~@body)))
+
 (defn slurp-resource [file]
   (slurp (clojure.java.io/resource file)))
 
 (defn parse [file]
-  (parse/parse (slurp-resource file)))
+  (consume-output
+    (parse/parse (slurp-resource file))))
 
 (defn should-fail? [file]
   (some? (re-matches #".*/fail_.*" file)))
@@ -18,9 +25,11 @@
   true)
 
 (defn valid-type? [file]
-  (type/check {:cells (parse file)})
+  (consume-output
+    (type/check {:cells (parse file)}))
   true)
 
 (defn valid-codegen? [file]
-  (codegen/codegen (first (type/check {:cells (parse file)})))
+  (consume-output
+    (codegen/codegen (first (type/check {:cells (parse file)}))))
   true)
