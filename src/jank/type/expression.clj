592416298a1891e75d4910008a60f1cb0cf5c9de
jeaye <contact@jeaye.com>
2015-12-21 22:34:14 +0800

    nyi: if expression type realization

--- src/jank/type/expression.clj
+++ src/jank/type/expression.clj
@@ -56,7 +56,7 @@
   ; Realizing the type of an if expression means that we need
   ; it for a value; in this case, we require both then and else clauses
   ; and they must evaluate to the same type.
-  nil)
+  (not-yet-implemented "if expression type realization"))
 
 (defmethod realize-type :list [item scope]
   ; TODO
