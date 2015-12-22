bc24dc9699b63741cf834abe501583857905899d
jeaye <contact@jeaye.com>
2015-12-22 13:49:33 +0800

    Add an arity overload to parse

--- src/jank/parse.clj
+++ src/jank/parse.clj
@@ -9,10 +9,12 @@
     (clojure.java.io/resource "grammar")
     :auto-whitespace :standard))
 
-(defn parse [pre resource]
-  "Runs the provided resource file through instaparse. Returns
-   then generated syntax tree."
-  (let [parsed (parser (str pre resource))]
-    (when (insta/failure? parsed)
-      (parse-assert false))
-    parsed))
+(defn parse
+  ([resource] (parse prelude resource))
+  ([pre resource]
+   "Runs the provided resource file through instaparse. Returns
+    then generated syntax tree."
+   (let [parsed (parser (str pre resource))]
+     (when (insta/failure? parsed)
+       (parse-assert false))
+     parsed)))
