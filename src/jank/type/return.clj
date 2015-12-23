c86d6c9a928987852d57e814c551174aafbba814
jeaye <contact@jeaye.com>
2015-12-23 23:39:23 +0800

    Finish adding explicit returns for if expressions

--- src/jank/type/return.clj
+++ src/jank/type/return.clj
@@ -15,25 +15,25 @@
 (defmethod add-explicit-returns :if-expression [item scope]
   (type-assert (= 4 (count item)) "no else statement")
 
-  (let [then-body (second (add-explicit-returns [:body (rest (nth item 2))] scope))
-        else-body (second (add-explicit-returns [:body (rest (nth item 3))] scope))]
+  (let [then-body (second (add-explicit-returns [:body (second (nth item 2))]
+                                                scope))
+        else-body (second (add-explicit-returns [:body (second (nth item 3))]
+                                                scope))]
     (type-assert (not-empty then-body) "no return value in if/then expression")
     (type-assert (not-empty else-body) "no return value in if/else expression")
 
     (let [then-type (expression/realize-type
-                      (add-explicit-returns (last then-body) scope)
+                      (add-explicit-returns then-body scope)
                       scope)
           else-type (expression/realize-type
-                      (add-explicit-returns (last else-body) scope)
+                      (add-explicit-returns else-body scope)
                       scope)]
       (type-assert (= then-type else-type)
                    "incompatible if then/else types")
-      ; TODO: Actually add in returns and update item
-      (let [updated-then-body (concat (butlast then-body)
-                                      [[:return (last then-body)]])
-            updated-else-body (concat (butlast else-body)
-                                      [[:return (last else-body)]])]
-        item))))
+      (update-in
+        (update-in item [2] (fn [_] [:then [:return then-body]]))
+        [3]
+        (fn [_] [:else [:return else-body]])))))
 
 (defmethod add-explicit-returns :body [item scope]
   item)
