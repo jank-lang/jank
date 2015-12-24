f23e83889b9fa72a70fb50d5b1a8317688e3b9ae
jeaye <contact@jeaye.com>
2015-12-24 22:46:04 +0800

    Remove some dead code

--- src/jank/type/return.clj
+++ src/jank/type/return.clj
@@ -20,10 +20,6 @@
                                                 scope))
         else-body (second (add-explicit-returns [:body (rest (nth item 3))]
                                                 scope))]
-    (internal-assert (not-empty then-body)
-                     "no return value in if/then expression")
-    (internal-assert (not-empty else-body)
-                     "no return value in if/else expression")
 
     (let [then-type (expression/realize-type (last then-body) scope)
           else-type (expression/realize-type (last else-body) scope)]
