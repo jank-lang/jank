54f8044243764c046a341c3102d8663ce140e1e1
jeaye <contact@jeaye.com>
2015-12-24 11:30:11 +0800

    Begin working toward a generic :body impl

--- src/jank/type/return.clj
+++ src/jank/type/return.clj
@@ -15,25 +15,25 @@
 (defmethod add-explicit-returns :if-expression [item scope]
   (type-assert (= 4 (count item)) "no else statement")
 
-  (let [then-body (second (add-explicit-returns [:body (second (nth item 2))]
+  (let [then-body (second (add-explicit-returns [:body (rest (nth item 2))]
                                                 scope))
-        else-body (second (add-explicit-returns [:body (second (nth item 3))]
+        else-body (second (add-explicit-returns [:body (rest (nth item 3))]
                                                 scope))]
     (type-assert (not-empty then-body) "no return value in if/then expression")
     (type-assert (not-empty else-body) "no return value in if/else expression")
 
     (let [then-type (expression/realize-type
-                      (add-explicit-returns then-body scope)
+                      (add-explicit-returns (last then-body) scope)
                       scope)
           else-type (expression/realize-type
-                      (add-explicit-returns else-body scope)
+                      (add-explicit-returns (last else-body) scope)
                       scope)]
       (type-assert (= then-type else-type)
                    "incompatible if then/else types")
       (update-in
-        (update-in item [2] (fn [_] [:then [:return then-body]]))
+        (update-in item [2] (fn [_] [:then [:return (last then-body)]]))
         [3]
-        (fn [_] [:else [:return else-body]])))))
+        (fn [_] [:else [:return (last else-body)]])))))
 
 (defmethod add-explicit-returns :body [item scope]
   item)
