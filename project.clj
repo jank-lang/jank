1205d9528d41bab0cb8a8ba87900464a23a41b0c
jeaye <contact@jeaye.com>
2015-12-30 11:09:34 +0800

    Revert "Version 0.1.0"
    This reverts commit eff6d4bd010d53cc3ba0af1528c73a0b0aacf67b.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jank "0.1.0"
+(defproject jank "0.1.0-SNAPSHOT"
   :description "A statically typed functional programming language"
   :url "https://github.com/jeaye/jank"
   :license {:name "MIT"
