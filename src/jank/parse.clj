4a14c1012fa66d3471de438fe01fbaf7bb2e7027
jeaye <contact@jeaye.com>
2016-03-15 23:59:03 -0700

    Transform the parse tree into maps first

--- src/jank/parse.clj
+++ src/jank/parse.clj
@@ -10,6 +10,17 @@
     (clojure.java.io/resource "grammar")
     :auto-whitespace :standard))
 
+(defn transform-single [kind value]
+  {:kind kind :value value})
+
+(defn transform-declare [& args]
+  (let [base {:kind :declare-statement
+              :type (last args)}
+        size (count args)]
+    (if (= 2 size) ; Has identifier and type
+      (assoc base :name (first args))
+      base)))
+
 (defn parse
   "Runs the provided resource file through instaparse. Returns
    then generated syntax tree."
@@ -19,4 +30,20 @@
          error (pr-str (insta/get-failure parsed))]
      (parse-assert (not (insta/failure? parsed))
                    "invalid syntax\n" error)
+     (pprint parsed)
+     (pprint
+     (insta/transform {:integer (partial transform-single :integer)
+                       :real (partial transform-single :real)
+                       :boolean (partial transform-single :boolean)
+                       :identifier (partial transform-single :identifier)
+                       :keyword (partial transform-single :keyword)
+                       :declare-statement transform-declare
+                       ;:lambda-definition pass
+                       ;:macro-definition pass
+                       ;:binding-definition pass
+                       ;:if-expression pass
+                       ;:function-call pass
+                       }
+                      parsed)
+       )
      parsed)))
