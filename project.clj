43cd6607548af43d2e9c2394ce13c36bb7592f14
jeaye <contact@jeaye.com>
2016-03-22 22:24:19 -0700

    Add trace tool dep

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [instaparse "1.4.1"]]
+                 [instaparse "1.4.1"]
+                 [org.clojure/tools.trace "0.7.9"]]
   :main ^:skip-aot jank.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
