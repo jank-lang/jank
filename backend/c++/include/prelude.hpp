7649553bcf07d4cc9ab9de05d6697bf3cdf63b3f
jeaye <contact@jeaye.com>
2016-04-05 22:44:18 -0700

    Refactor out prelude arithmetic

--- backend/c++/include/prelude.hpp
+++ backend/c++/include/prelude.hpp
@@ -6,35 +6,4 @@
 #include "prelude/assert.hpp"
 #include "prelude/output.hpp"
 #include "prelude/input.hpp"
-
-namespace jank
-{
-  /* -- Primitive arithmetic -- */
-  template <typename A, typename B>
-  auto _gen_plus(A const &a, B const &b)
-  { return a + b; }
-
-  template <typename A, typename B>
-  auto _gen_minus(A const &a, B const &b)
-  { return a - b; }
-
-  auto operator *(string s, integer const n)
-  {
-    assert_bang(n > 0, "invalid scalar for string repetition");
-    auto const original_size(s.size());
-    s.reserve(s.size() * n);
-
-    for(integer i{ 1 }; i < n; ++i)
-    { s.insert(original_size * i, s.c_str(), original_size); }
-
-    return s;
-  }
-
-  template <typename A, typename B>
-  auto _gen_asterisk(A const &a, B const &b)
-  { return a * b; }
-
-  template <typename A, typename B>
-  auto _gen_slash(A const &a, B const &b)
-  { return a / b; }
-}
+#include "prelude/arithmetic.hpp"
