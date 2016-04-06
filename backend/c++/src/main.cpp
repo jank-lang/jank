ffd2225d8fdc75c1b57587ac17d83c5d23fcd7e4
jeaye <contact@jeaye.com>
2016-04-05 22:40:51 -0700

    Tweak codegen to call into jank_gen

--- backend/c++/src/main.cpp
+++ backend/c++/src/main.cpp
@@ -1,6 +1,6 @@
 #include "prelude.hpp"
 
-namespace jank
+namespace jank_gen
 {
 /* This is the generated source. */
 #include "jank-generated.hpp"
@@ -9,7 +9,7 @@ namespace jank
 int main(int const argc, char ** const argv)
 try
 {
-  jank::_gen_poundmain();
+  jank_gen::_gen_poundmain();
 }
 catch(std::exception const &e)
 { std::cout << "exception: " << e.what() << std::endl; }
