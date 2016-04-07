3cd3b6a85ca49db5ee5440e208347ef1e5d26971
jeaye <contact@jeaye.com>
2016-04-06 22:32:35 -0700

    Remove assert prelude mangled wrappers

--- backend/c++/include/prelude/assert.hpp
+++ backend/c++/include/prelude/assert.hpp
@@ -18,17 +18,3 @@ namespace jank
   void assert_unreachable_bang()
   { assert_bang(false, "unreachable code reached"); }
 }
-
-namespace jank_gen
-{
-  void assert_gen_bang1566167926(boolean const b, string const &s)
-  { jank::assert_bang(b, s); }
-  void assert_gen_bang_gen_minus1906603266(boolean const b)
-  { jank::assert_bang(b, ""); }
-  void assert_gen_minusnot_gen_bang_gen_minus1906603266(boolean const b)
-  { jank::assert_not_bang(b); }
-  void assert_gen_minusnot_gen_bang1566167926(boolean const b, string const &s)
-  { jank::assert_not_bang(b, s); }
-  void assert_gen_minusunreachable_gen_bang2018684456()
-  { jank::assert_unreachable_bang(); }
-}
