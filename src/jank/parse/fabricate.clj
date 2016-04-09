700ab916a4a4ca8dfa83ac73d0678a53548e0677
jeaye <contact@jeaye.com>
2016-04-09 12:28:49 -0700

    Add easy fabrication of parse tree nodes

---
+++ src/jank/parse/fabricate.clj
@@ -0,0 +1,29 @@
+(ns jank.parse.fabricate
+  (:refer-clojure :exclude [type])
+  (:use clojure.pprint
+        jank.assert))
+
+(defn type
+  [name]
+  {:kind :type
+   :value {:kind :identifier
+           :name name}})
+
+(defn function-declaration
+  [fn-name fn-args fn-ret]
+  {:kind :declare-statement
+   :external? false
+   :name {:kind :identifier
+          :name fn-name}
+   :type
+   {:kind :type
+    :value
+    {:kind :identifier
+     :name "ƒ"
+     :generics
+     {:kind :specialization-list
+      :values
+      [{:kind :specialization-list
+        :values (map type fn-args)}
+       {:kind :specialization-list
+        :values [(type fn-ret)]}]}}}})
