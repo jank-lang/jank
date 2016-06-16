366da367606fa3256094f49b0abc0e8c0a75e616
jeaye <contact@jeaye.com>
2016-06-15 23:23:17 -0700

    Clean up redundant .jank in parse tests

--- test/jank/parse_test.clj
+++ test/jank/parse_test.clj
@@ -4,120 +4,120 @@
 
 (def error #"parse error:")
 
-; TODO: Remove .jank from each and add it automatically
 (defn test-file [file]
-  (println "[parse] testing" file)
-  (if (should-fail? file)
-    (is (thrown-with-msg? AssertionError
-                          error
-                          (valid-parse? file)))
-    (is (valid-parse? file))))
+  (let [full-file (str file ".jank")]
+    (println "[parse] testing" full-file)
+    (if (should-fail? full-file)
+      (is (thrown-with-msg? AssertionError
+                            error
+                            (valid-parse? full-file)))
+      (is (valid-parse? full-file)))))
 
 ; TODO: Split into multiple tests
 (deftest comments
-  (doseq [file ["multi-line/fail-double-close.jank"
-                "multi-line/fail-no-close.jank"
-                "multi-line/pass-normal.jank"
-                "multi-line/pass-parens.jank"
-                "multi-line/pass-quotes.jank"
-                "multi-line/pass-unicode.jank"
-                "nested/fail-multi-line-multi-end.jank"
-                "nested/fail-no-close.jank"
-                "nested/fail-single-line-multi-end.jank"
-                "nested/pass-multi-line.jank"
-                "nested/fail-multi-line-multi-start.jank"
-                "nested/pass-single-line.jank"
-                "nested/fail-single-line-multi-start.jank"
-                "single-line/fail-double-close.jank"
-                "single-line/fail-no-close.jank"
-                "single-line/pass-multiple-in-one-file.jank"
-                "single-line/pass-normal.jank"
-                "single-line/pass-parens.jank"
-                "single-line/pass-quotes.jank"
-                "single-line/pass-unicode.jank"]]
+  (doseq [file ["multi-line/fail-double-close"
+                "multi-line/fail-no-close"
+                "multi-line/pass-normal"
+                "multi-line/pass-parens"
+                "multi-line/pass-quotes"
+                "multi-line/pass-unicode"
+                "nested/fail-multi-line-multi-end"
+                "nested/fail-no-close"
+                "nested/fail-single-line-multi-end"
+                "nested/pass-multi-line"
+                "nested/fail-multi-line-multi-start"
+                "nested/pass-single-line"
+                "nested/fail-single-line-multi-start"
+                "single-line/fail-double-close"
+                "single-line/fail-no-close"
+                "single-line/pass-multiple-in-one-file"
+                "single-line/pass-normal"
+                "single-line/pass-parens"
+                "single-line/pass-quotes"
+                "single-line/pass-unicode"]]
     (test-file (str "test/parse/comment/" file))))
 
 (deftest idents
-  (doseq [file ["ascii/fail-bad-chars.jank"
-                "ascii/pass-good-chars.jank"
-                "ascii/pass-true-false.jank"
-                "unicode/pass-all-good.jank"]]
+  (doseq [file ["ascii/fail-bad-chars"
+                "ascii/pass-good-chars"
+                "ascii/pass-true-false"
+                "unicode/pass-all-good"]]
     (test-file (str "test/parse/ident/" file))))
 
 (deftest parens
-  (doseq [file ["match/fail-close-nothing-else.jank"
-                "match/fail-multiple-close-nothing-else.jank"
-                "match/fail-multiple-open-nothing-else.jank"
-                "match/fail-open-nothing-else.jank"]]
+  (doseq [file ["match/fail-close-nothing-else"
+                "match/fail-multiple-close-nothing-else"
+                "match/fail-multiple-open-nothing-else"
+                "match/fail-open-nothing-else"]]
     (test-file (str "test/parse/paren/" file))))
 
 (deftest strings
-  (doseq [file ["escape/pass-lots-of-unescaped-closes.jank"
-                "escape/pass-unescaped-both.jank"
-                "escape/pass-unescaped-close.jank"
-                "escape/pass-unescaped-open.jank"
-                "escape/pass-escape-both.jank"
-                "escape/pass-escape-close.jank"
-                "escape/pass-escape-open.jank"]]
+  (doseq [file ["escape/pass-lots-of-unescaped-closes"
+                "escape/pass-unescaped-both"
+                "escape/pass-unescaped-close"
+                "escape/pass-unescaped-open"
+                "escape/pass-escape-both"
+                "escape/pass-escape-close"
+                "escape/pass-escape-open"]]
     (test-file (str "test/parse/string/" file))))
 
 (deftest bindings
-  (doseq [file ["fail-missing-value.jank"
-                "pass-builtin-function.jank"
-                "pass-builtin-identifier.jank"
-                "pass-builtin-literal.jank"
-                "pass-builtin-parameter.jank"]]
+  (doseq [file ["fail-missing-value"
+                "pass-builtin-function"
+                "pass-builtin-identifier"
+                "pass-builtin-literal"
+                "pass-builtin-parameter"]]
     (test-file (str "test/parse/binding/" file))))
 
 (deftest ifs
-  (doseq [file ["fail-without-both.jank"
-                "fail-too-many-branches.jank"
-                "pass-with-literal.jank"
-                "pass-without-else.jank"
-                "pass-with-predicate.jank"]]
+  (doseq [file ["fail-without-both"
+                "fail-too-many-branches"
+                "pass-with-literal"
+                "pass-without-else"
+                "pass-with-predicate"]]
     (test-file (str "test/parse/if/" file))))
 
 (deftest lambda-definitions
-  (doseq [file ["fail-missing-param-name.jank"
-                "fail-no-param-list.jank"
-                "fail-no-return-type.jank"
-                "fail-multiple-return-types.jank"
-                "pass-body.jank"
-                "pass-empty.jank"
-                "pass-primitive.jank"
-                "pass-long-form.jank"
-                "pass-unicode-short-form.jank"]]
+  (doseq [file ["fail-missing-param-name"
+                "fail-no-param-list"
+                "fail-no-return-type"
+                "fail-multiple-return-types"
+                "pass-body"
+                "pass-empty"
+                "pass-primitive"
+                "pass-long-form"
+                "pass-unicode-short-form"]]
     (test-file (str "test/parse/lambda/define/" file))))
 
 (deftest function-calls
-  (doseq [file ["fail-non-function-literal.jank"]]
+  (doseq [file ["fail-non-function-literal"]]
     (test-file (str "test/parse/function/call/" file))))
 
 ; TODO: Split these into type/binding directories
 (deftest declarations
-  (doseq [file ["fail-missing-idenfitier.jank"
-                "fail-non-identifier-integer.jank"
-                "fail-non-identifier-string.jank"
+  (doseq [file ["fail-missing-idenfitier"
+                "fail-non-identifier-integer"
+                "fail-non-identifier-string"
                 ; TODO: Generics
-                ;"fail-empty-generic-type.jank"
-                ;"pass-generic-type-with-parameters.jank"
-                "pass-type.jank"
-                "pass-binding.jank"
-                "pass-generic-binding.jank"]]
+                ;"fail-empty-generic-type"
+                ;"pass-generic-type-with-parameters"
+                "pass-type"
+                "pass-binding"
+                "pass-generic-binding"]]
     (test-file (str "test/parse/declaration/" file))))
 
 (deftest structs
-  (doseq [file ["fail-no-members.jank"
-                "fail-empty-member.jank"
-                "fail-member-too-many-fields.jank"
-                "fail-no-name.jank"
-                "fail-no-name-some-members.jank"
-                "fail-too-many-names.jank"
-                "fail-non-list-member.jank"
-                "pass-one-member-no-value.jank"
-                "pass-one-member-with-value.jank"
-                "pass-unicode-name.jank"
-                "pass-multiple-members-no-values.jank"
-                "pass-multiple-members-all-values.jank"
-                "pass-multiple-members-mixed-values.jank"]]
+  (doseq [file ["fail-no-members"
+                "fail-empty-member"
+                "fail-member-too-many-fields"
+                "fail-no-name"
+                "fail-no-name-some-members"
+                "fail-too-many-names"
+                "fail-non-list-member"
+                "pass-one-member-no-value"
+                "pass-one-member-with-value"
+                "pass-unicode-name"
+                "pass-multiple-members-no-values"
+                "pass-multiple-members-all-values"
+                "pass-multiple-members-mixed-values"]]
     (test-file (str "test/parse/struct/" file))))
