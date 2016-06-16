cbf49cfd6c7899944adf8195e3342929b6ea2010
jeaye <contact@jeaye.com>
2016-06-15 22:49:30 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/jeaye/jank"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [instaparse "1.4.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [instaparse "1.4.2"]
                  [org.clojure/tools.trace "0.7.9"]
                  [me.raynes/fs "1.4.6"]]
   :main ^:skip-aot jank.core
