86ba33bd74153ae256cfa51e174a5c4cd78e64ba
jeaye <contact@jeaye.com>
2016-08-21 11:28:24 -0700

    Update license info

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject jank "0.1.0-SNAPSHOT"
   :description "A statically typed functional programming language"
   :url "https://github.com/jeaye/jank"
-  :license {:name "MIT"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:name "jank license"
+            :url "https://upload.jeaye.com/jank-license"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [instaparse "1.4.2"]
                  [org.clojure/core.logic "0.8.10"]
