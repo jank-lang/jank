0917af78547e6aef2b1f559e78b458be3503ece3
jeaye <contact@jeaye.com>
2016-10-10 22:26:27 -0700

    Simplify interpreter prelude

--- src/jank/interpret/macro.clj
+++ src/jank/interpret/macro.clj
@@ -2,22 +2,23 @@
   (:use jank.assert
         jank.debug.log))
 
-(def prelude {:functions {{:name "print!"
-                           :argument-types [:string]} pprint
-                          {:name "print!"
-                           :argument-types [:integer]} pprint
-                          {:name "print!"
-                           :argument-types [:real]} pprint
-                          {:name "print!"
-                           :argument-types [:boolean]} pprint}})
+(def prelude {{:name "print!"
+                :argument-types [:string]} pprint
+               {:name "print!"
+                :argument-types [:integer]} pprint
+               {:name "print!"
+                :argument-types [:real]} pprint
+               {:name "print!"
+                :argument-types [:boolean]} pprint})
 
 (defmulti evaluate-item
   (fn [item env]
     (:kind item)))
 
 (defn evaluate
-  ([body] (evaluate body prelude))
+  ([item] (evaluate [item] prelude))
   ([body env]
+   (pprint (clean-scope body))
    (reduce #(let [item (evaluate-item %2 (:env %1))]
               (assoc %1
                      :cells (conj (:cells %1) item)
@@ -28,11 +29,16 @@
 
 (defmethod evaluate-item :macro-call
   [item env]
-  ;(pprint "evaluating macro " (clean-scope item) env)
+  (pprint "evaluating macro " (clean-scope item) env)
   ; TODO: if external, the function must be in prelude
   ; TODO: Add arguments to env
   ; TODO: (assoc item [:interpreted :value] ...)
-  (let [body (evaluate (get-in item [:definition :body]) env)]
+  (let [;arg-types (map #(expression/realize-type % (:scope item))
+        ;               (:arguments))
+        ;env-with-args (reduce #(assoc %1 )
+        ;                      env
+        ;                      (:arguments item))
+        body (evaluate (get-in item [:definition :body]) env)]
     (-> (assoc-in item [:definition :body] (:cells body))
         (assoc :env (:env body)))))
 
@@ -42,7 +48,7 @@
   (let [signature {:name (-> item :name :name)
                    :argument-types (map :kind (:arguments item))}
         arguments (map #(evaluate-item % env) (:arguments item))
-        func (get (:functions env) signature)]
+        func (get env signature)]
     (interpret-assert func (str "unknown function " signature))
     (apply func (map :value arguments))
     (assoc item :env env)))
@@ -64,6 +70,11 @@
   (assoc item :env env))
 
 (defmethod evaluate-item :identifier
+  [item env]
+  ; TODO
+  (assoc item :env env))
+
+(defmethod evaluate-item :return
   [item env]
   ; TODO
   (assoc item :env env))
