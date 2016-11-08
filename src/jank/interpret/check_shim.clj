a13bb0df88ecf4066068ae14593796f6211508ab
jeaye <contact@jeaye.com>
2016-11-07 23:06:44 -0800

    Try out declare-extern

--- src/jank/interpret/check_shim.clj
+++ src/jank/interpret/check_shim.clj
@@ -1,10 +1,13 @@
 (ns jank.interpret.check-shim
-  ;(:require [jank.type.check :as check]) ; TODO: cyclical
+  (:require [jank.clojure :as clj])
   (:use jank.assert
         jank.debug.log))
+
+(clj/declare-extern jank.core/check)
 
 (defn check
   "Takes a syntax definition, converts it to a string, reparses it as normal
    code, and type checks it. Returns the checked body in a syntax definition."
   [syntax-def]
-  (pprint "checking syntax" syntax-def))
+  (pprint "checking syntax" syntax-def)
+  (jank.core/check syntax-def))
