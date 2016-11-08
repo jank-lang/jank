378cf66d030ea61e144ee3cdaa02ec818a086788
jeaye <contact@jeaye.com>
2016-11-07 23:07:05 -0800

    Revert "Try out declare-extern"
    This reverts commit a13bb0df88ecf4066068ae14593796f6211508ab.

--- src/jank/interpret/check_shim.clj
+++ src/jank/interpret/check_shim.clj
@@ -1,13 +1,10 @@
 (ns jank.interpret.check-shim
-  (:require [jank.clojure :as clj])
+  ;(:require [jank.type.check :as check]) ; TODO: cyclical
   (:use jank.assert
         jank.debug.log))
-
-(clj/declare-extern jank.core/check)
 
 (defn check
   "Takes a syntax definition, converts it to a string, reparses it as normal
    code, and type checks it. Returns the checked body in a syntax definition."
   [syntax-def]
-  (pprint "checking syntax" syntax-def)
-  (jank.core/check syntax-def))
+  (pprint "checking syntax" syntax-def))
