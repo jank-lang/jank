f7d514d0430796a9c6fd38cd773ca9b3eae5df45
jeaye <contact@jeaye.com>
2016-11-10 21:25:33 -0800

    Outline check shim implementation

--- src/jank/interpret/check_shim.clj
+++ src/jank/interpret/check_shim.clj
@@ -6,4 +6,7 @@
   "Takes a syntax definition, converts it to a string, reparses it as normal
    code, and type checks it. Returns the checked body in a syntax definition."
   [actual-check syntax-def]
+  ; TODO: Convert syntax def to string
+  ; TODO: Parse/transform string into tree
+  ; TODO: Type check tree with current scope (have evaluate take in scope?)
   (pprint "checking syntax" syntax-def))
