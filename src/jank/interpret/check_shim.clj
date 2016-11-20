49924011a6d4f31b81bfc09c0ce1464a23d2b5cf
jeaye <contact@jeaye.com>
2016-11-19 16:30:28 -0800

    Check shim macros with marked scope

--- src/jank/interpret/check_shim.clj
+++ src/jank/interpret/check_shim.clj
@@ -1,5 +1,6 @@
 (ns jank.interpret.check-shim
   (:require [jank.parse :as parse]
+            [jank.type.scope.util :as scope.util]
             [clojure.walk :as walk]
             [clojure.string :as string])
   (:use jank.assert
@@ -23,6 +24,12 @@
   [syntax-def]
   (apply str (walk/postwalk unparse-item syntax-def)))
 
+(defn new-macro-scope
+  "Returns a scope that's marked as being in a macro"
+  [parent]
+  (assoc (scope.util/new-empty parent)
+         :in-macro? true))
+
 (defn check
   "Takes a syntax definition, converts it to a string, reparses it as normal
    code, and type checks it. Returns the checked body in a syntax definition."
@@ -30,6 +37,6 @@
   (let [unparsed (unparse syntax-def)
         ; Empty prelude, since the scope already includes it
         parsed (parse/parse "" unparsed)
-        ; Use the parent scope so we can't pull in macro bindings
-        checked (actual-check {:cells parsed} (:parent scope))]
+        checked (actual-check {:cells parsed}
+                              (new-macro-scope scope))]
     checked))
