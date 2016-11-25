9ec86df65205a12682d5fce688a4d4e191a0df3b
jeaye <contact@jeaye.com>
2016-11-25 15:42:56 -0800

    Refactor out struct type tests

--- test/jank/test/type/util.clj
+++ test/jank/test/type/util.clj
@@ -14,24 +14,6 @@
       (is (valid-type? full-file)))))
 
 
-(deftest structs
-  ; TODO: Fix these
-  (doseq [file [;"fail-name-used"
-                "fail-multiple-definition"
-                "fail-invalid-member-type"
-                "fail-members-same-name"
-                ;"fail-member-function-redefinition"
-                ;"fail-member-function-declaration-incorrect"
-                ;"fail-extern-declaration"
-                "pass-single-member"
-                "pass-multiple-members"
-                "pass-extern-type-member"
-                "pass-struct-member"
-                "pass-recursive"
-                "pass-declaration"
-                "pass-member-declaration"]]
-    (test-file (str "test/type/struct/" file))))
-
 (deftest new-expressions
   (doseq [file ["fail-incorrect-value-types"
                 "fail-no-values"
