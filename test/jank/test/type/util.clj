916562cf44d82ebfe3b7a535583aee3be19fd072
jeaye <contact@jeaye.com>
2016-11-25 15:43:45 -0800

    Refactor out new expression type tests

--- test/jank/test/type/util.clj
+++ test/jank/test/type/util.clj
@@ -12,19 +12,3 @@
                             error
                             (valid-type? full-file)))
       (is (valid-type? full-file)))))
-
-
-(deftest new-expressions
-  (doseq [file ["fail-incorrect-value-types"
-                "fail-no-values"
-                "fail-not-enough-values"
-                "fail-too-many-types"
-                "fail-too-many-values"
-                "fail-unknown-type"
-                "pass-as-parameter"
-                "pass-constructor"
-                "pass-correct-values"
-                "pass-value-expressions"
-                "pass-binding-explicit"
-                "pass-binding-implicit"]]
-    (test-file (str "test/type/new/" file))))
