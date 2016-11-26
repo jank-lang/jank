792599e3a5c7379676c55fa1096526d5eaccaf41
jeaye <contact@jeaye.com>
2016-11-25 22:33:35 -0800

    Update parse tests for relaxed ' allowance

--- test/jank/test/parse/ident.clj
+++ test/jank/test/parse/ident.clj
@@ -3,8 +3,7 @@
             [jank.test.parse.util :as util]))
 
 (deftest ident
-  (doseq [file ["ascii/fail-bad-chars"
-                "ascii/pass-good-chars"
+  (doseq [file ["ascii/pass-good-chars"
                 "ascii/pass-true-false"
                 "unicode/pass-all-good"]]
     (util/test-file (str "test/parse/ident/" file))))
