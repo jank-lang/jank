a336493ad2bdceac5cb1b29ee252e6a8f6bb10ac
jeaye <contact@jeaye.com>
2016-11-27 00:53:43 -0800

    Add <= to static prelude

--- backend/c++/include/prelude/equality.hpp
+++ backend/c++/include/prelude/equality.hpp
@@ -7,4 +7,8 @@ namespace jank
   template <typename LHS, typename RHS>
   auto _gen_equal(LHS &&lhs, RHS &&rhs)
   { return lhs == rhs; }
+
+  template <typename LHS, typename RHS>
+  auto _gen_less_gen_equal(LHS &&lhs, RHS &&rhs)
+  { return lhs <= rhs; }
 }
