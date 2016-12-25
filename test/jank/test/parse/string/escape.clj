a9b413336a4597e0d271239adca9e22b8e6699e5
jeaye <contact@jeaye.com>
2016-12-24 22:04:09 -0800

    Adjust some test paths

--- test/jank/test/parse/string/escape.clj
+++ test/jank/test/parse/string/escape.clj
@@ -3,11 +3,11 @@
             [jank.test.parse.util :as util]))
 
 (deftest string-escape
-  (doseq [file ["escape/pass-lots-of-unescaped-closes"
-                "escape/pass-unescaped-both"
-                "escape/pass-unescaped-close"
-                "escape/pass-unescaped-open"
-                "escape/pass-escape-both"
-                "escape/pass-escape-close"
-                "escape/pass-escape-open"]]
+  (doseq [file ["pass-lots-of-unescaped-closes"
+                "pass-unescaped-both"
+                "pass-unescaped-close"
+                "pass-unescaped-open"
+                "pass-escape-both"
+                "pass-escape-close"
+                "pass-escape-open"]]
     (util/test-file (str "test/parse/string/escape/" file))))
