63f6c550cb24f2a4e143ebfb9712dd439475c488
jeaye <contact@jeaye.com>
2016-12-26 12:28:44 -0800

    Wrap bootstrap bits in reload for repl

--- dev/jank/dev.clj
+++ dev/jank/dev.clj
@@ -2,10 +2,20 @@
   (:gen-class)
   (:require [jank.core :as core]
             [jank.parse :as parse]
-            [jank.test.bootstrap :as bootstrap :refer :all]))
+            [jank.test.bootstrap :as bootstrap]))
 
 (defn reload []
   (use 'jank.dev :reload-all))
+
+(defmacro def-reload
+  [def-name to-wrap]
+  `(defn ~def-name [& args#]
+     (reload)
+     (apply ~to-wrap args#)))
+
+(def-reload try-parse bootstrap/try-parse)
+(def-reload try-type bootstrap/try-type)
+(def-reload try-interpret bootstrap/try-interpret)
 
 (defn run
   "Analogous to lein run; start jank with the specified args"
@@ -18,6 +28,7 @@
   ([file & args]
    (reload)
    (apply parse/parses (slurp file) args)))
+
 
 (defn -main [& args]
   (apply run args))
