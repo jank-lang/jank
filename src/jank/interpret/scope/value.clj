a1da48ada2ce380b977fbbde6388fcad43211f9d
jeaye <contact@jeaye.com>
2016-12-29 11:02:03 -0800

    Document next todo steps for value storage

--- src/jank/interpret/scope/value.clj
+++ src/jank/interpret/scope/value.clj
@@ -5,6 +5,11 @@
 
 (def lookup (partial util/lookup #(get (:interpreted-values %2) %1)))
 
+; TODO: Rework how values are stored
+; 1. give each new-scope an id, from incrementing an atom
+; 2. store values in a map of name and id to map of types to values
+; 3. pass value map and type scope as separate items
+
 (defn add-to-scope
   [item-name item-type value scope]
   (let [dest-path [:interpreted-values item-name item-type]]
