bdfbe61fe16dc8dac4c45f9f7af1ad0142481198
jeaye <contact@jeaye.com>
2016-12-31 17:32:04 -0800

    Update clj-time to latest

--- project.clj
+++ project.clj
@@ -7,14 +7,14 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [instaparse "1.4.5"]
                  [org.clojure/core.logic "0.8.11"]
-                 [me.raynes/fs "1.4.6"]
-                 [clj-time "0.12.2"]]
+                 [me.raynes/fs "1.4.6"]]
   :main ^:skip-aot jank.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev/"]
                    :main jank.dev}
-             :benchmark {:dependencies [[criterium "0.4.4"]]
+             :benchmark {:dependencies [[criterium "0.4.4"]
+                                        [clj-time "0.13.0"]]
                          :source-paths ["benchmark/"]
                          :resource-paths ["dev-resources/"]
                          :main jank.benchmark}})
