17fb11a1147bddbaf905773ae39f55afbcf738ef
jeaye <contact@jeaye.com>
2017-01-12 21:03:43 -0800

    Assert complete instantiations

--- src/jank/type/generic.clj
+++ src/jank/type/generic.clj
@@ -13,12 +13,12 @@
 
 (defn instantiate [call scope]
   (let [matches (expression/overload-matches call scope)
-        match (ffirst (:full-matches matches))]
+        match (ffirst (:partial-matches matches))]
     (pprint "call" call)
     (pprint "match" match)
     (if-not (contains? match :generics)
       call
-      (let [generic-types (-> match :generics)
+      (let [generic-types (-> match :generics :values)
             ; TODO: Support explicit param specification
             expected-argument-types (-> match
                                         :value :generics
@@ -29,13 +29,19 @@
             expected-actual-pairs (map vector
                                        expected-argument-types
                                        actual-argument-types)
+            empty-type-mapping (zipmap generic-types (repeat nil))
             argument-type-mapping (reduce map-type
-                                          {}
+                                          empty-type-mapping
                                           expected-actual-pairs)
+            _ (pprint "mapping" argument-type-mapping)
             ;return-types (-> match :value :generics :values second :values)
             ; TODO: Navigate through arguments and setup type mapping
             ]
-        ; TODO: Assert all types have a mapping
+        (type-assert (every? (comp some? second) argument-type-mapping)
+                     (str "incomplete instantiation of "
+                          (-> call :name :name)
+                          " with type mapping "
+                          argument-type-mapping))
         ; TODO: Substitute actual types in, for return type too
         ; TODO: Assoc instantiation info onto call
         ))))
