01a34d656847447a2504c1840f3bc0a3bfc745bc
jeaye <contact@jeaye.com>
2017-06-22 23:13:25 -0700

    Online the logic programming steps

--- playground/jank/neo/core.clj
+++ playground/jank/neo/core.clj
@@ -2,6 +2,11 @@
   (:require [jank.debug.log :refer [pprint]]
             [clojure.core.logic :as l]))
 
+; 1. Type-check simple calls
+; 2. Overload resolution
+; 3. Check body of function with arguments
+; 4. Ask what types a generic function could take, based on its body
+
 (def types [:int :float :string])
 
 (def foo-fn {:args [:int :float]
