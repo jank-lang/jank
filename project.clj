4c557a7fe66619eaf70a5a10954a9159bf46943c
jeaye <contact@jeaye.com>
2018-12-09 18:52:37 -0800

    Add lein-native-image and related config

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/jeaye/jank"
   :license {:name "jank license"
             :url "https://upload.jeaye.com/jank-license"}
-  :plugins [[lein-cloverage "1.0.10"]]
+  :plugins [[lein-cloverage "1.0.10"]
+            [io.taylorwood/lein-native-image "0.2.0"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.logic "0.8.11"]
                  [instaparse "1.4.8"]
@@ -14,7 +15,13 @@
                  [venantius/glow "0.1.4"]]
   :main ^:skip-aot jank.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
+  :native-image {:name "jank"
+                 :opts ["--verbose"
+                        "--no-server"
+                        "-H:+ReportUnsupportedElementsAtRuntime"]
+                 :graal-bin "/usr/lib/jvm/java-8-graal/bin"}
+  :profiles {:uberjar {:aot :all
+                       :main com.jeaye.jank.core}
              :dev {:source-paths ["dev/" "playground/"]
                    :main jank.dev}
              :coverage {:resource-paths ["dev-resources/"]}
