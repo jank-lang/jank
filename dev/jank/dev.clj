d475213c126c0f378555eecc49b2d2ea0370ddf7
jeaye <contact@jeaye.com>
2019-01-09 23:34:12 -0800

    Use neo jank in jank.dev

--- dev/jank/dev.clj
+++ dev/jank/dev.clj
@@ -1,32 +1,30 @@
 (ns jank.dev
   (:gen-class)
-  (:require [jank.core :as core]
-            [jank.parse :as parse]
-            [jank.test.bootstrap :as bootstrap]))
+  (:require [com.jeaye.jank.core :as core]
+            [com.jeaye.jank.parse :as parse]
+            [com.jeaye.jank.test.bootstrap :as bootstrap]))
 
-(defn reload []
+(defn reload! []
   (use 'jank.dev :reload-all))
 
 (defmacro def-reload
   [def-name to-wrap]
   `(defn ~def-name [& args#]
-     (reload)
+     (reload!)
      (apply ~to-wrap args#)))
 
 (def-reload parse bootstrap/try-parse)
-(def-reload type-check bootstrap/try-type-check)
-(def-reload interpret bootstrap/try-interpret)
 
 (defn run
   "Analogous to lein run; start jank with the specified args"
   [& args]
-  (reload)
+  (reload!)
   (apply core/-main args))
 
 (defn parses
   ([file] (parses file {}))
   ([file & args]
-   (reload)
+   (reload!)
    (apply parse/parses (slurp file) args)))
 
 (defn -main [& args]
