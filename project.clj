9b3d2c5ab7463b412b4716ca758b15045ef2e26d
jeaye <contact@jeaye.com>
2019-04-27 10:58:40 -0700

    Trim and upgrade deps

--- project.clj
+++ project.clj
@@ -3,24 +3,18 @@
   :url "https://github.com/jeaye/jank"
   :license {:name "jank license"
             :url "https://upload.jeaye.com/jank-license"}
-  :plugins [[lein-cloverage "1.0.13"]
-            [io.taylorwood/lein-native-image "0.3.0"]
-            [cider/cider-nrepl "0.20.0"]
-            [refactor-nrepl "2.4.0"]]
+  :plugins [[lein-cloverage "1.1.1"]
+            [io.taylorwood/lein-native-image "0.3.0"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/spec.alpha "0.2.176"]
                  [org.clojure/test.check "0.9.0"]
                  [instaparse "1.4.10"]
-                 [orchestra "2018.12.06-2"]
+                 [orchestra "2019.02.06-1"]
                  [expound "0.7.2"]
 
                  ; Colored output
-                 [io.aviso/pretty "0.1.36"]
-                 [venantius/glow "0.1.5"]
-
-                 ; REPL
-                 [alembic "0.3.2"]
-                 [nrepl "0.5.3"] ]
+                 [io.aviso/pretty "0.1.37"]
+                 [venantius/glow "0.1.5"]]
   :main ^:skip-aot com.jeaye.jank.core
   :target-path "target/%s"
   :native-image {:name "jank"
@@ -32,7 +26,7 @@
   :profiles {:uberjar {:aot :all
                        :main com.jeaye.jank.core}
              :dev {:dependencies [[me.raynes/fs "1.4.6"]]
-                   :plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]]
+                   :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
                    :source-paths ["dev/src/"]
                    :resource-paths ["dev/resources/"]
                    :main com.jeaye.jank.dev}})
