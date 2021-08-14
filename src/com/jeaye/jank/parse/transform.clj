762e9d3be7db47c314e015ae24c20ab51bb4108b
jeaye <contact@jeaye.com>
2021-08-14 12:19:27 -0700

    Remove deftransform macro; use a fn

--- src/com/jeaye/jank/parse/transform.clj
+++ src/com/jeaye/jank/parse/transform.clj
@@ -12,136 +12,161 @@
 (defn merge-meta [obj new-meta]
   (with-meta obj (merge (meta obj) new-meta)))
 
-; TODO: Switch to with-transform and use normal defns
-(defmacro deftransform [fn-name fn-args & fn-body]
-  `(defn ~fn-name ~fn-args
-     (-> (binding [parse.binding/*current-form* (merge-meta parse.binding/*current-form*
-                                                            {:file ~'parse.binding/*input-file*})]
-           ~@fn-body)
-         (merge-meta {:file ~'parse.binding/*input-file*}))))
+(defn with-transform-meta [fun]
+  (-> (binding [parse.binding/*current-form* (merge-meta parse.binding/*current-form*
+                                                         {:file parse.binding/*input-file*})]
+        (fun))
+      (merge-meta {:file parse.binding/*input-file*})))
 
-(deftransform constant [transformer & args]
-  (let [transformed (apply transformer args)]
-    (assoc transformed
-           ::parse.spec/kind :constant
-           ::parse.spec/type (::parse.spec/kind transformed))))
+(defn constant [transformer & args]
+  (with-transform-meta
+    #(let [transformed (apply transformer args)]
+       (assoc transformed
+              ::parse.spec/kind :constant
+              ::parse.spec/type (::parse.spec/kind transformed)))))
 
-(deftransform none [kind]
-  {::parse.spec/kind kind})
+(defn none [kind]
+  (with-transform-meta
+    #(do
+       {::parse.spec/kind kind})))
 
 ; TODO: Rename value to node? the whole thing is the node though
-(deftransform single [kind value]
-  {::parse.spec/kind kind
-   ::parse.spec/value value})
+(defn single [kind value]
+  (with-transform-meta
+    #(do
+       {::parse.spec/kind kind
+        ::parse.spec/value value})))
 
-(deftransform single-values [kind values]
-  {::parse.spec/kind kind
-   ::parse.spec/values values})
+(defn single-values [kind values]
+  (with-transform-meta
+    #(do
+       {::parse.spec/kind kind
+        ::parse.spec/values values})))
 
-(deftransform read-single [kind value]
-  {::parse.spec/kind kind
-   ::parse.spec/value (edn/read-string value)})
+(defn read-single [kind value]
+  (with-transform-meta
+    #(do
+       {::parse.spec/kind kind
+        ::parse.spec/value (edn/read-string value)})))
 
-(deftransform keyword [qualified & more]
-  (let [qualified? (= qualified :qualified)]
-    (merge {::parse.spec/kind :keyword}
-           (cond
-             qualified?
-             {::parse.spec/ns (second more)
-              ::parse.spec/name (nth more 3)}
-             (= (first more) "::")
-             {::parse.spec/ns ::parse.spec/current
-              ::parse.spec/name (second more)}
-             :else
-             {::parse.spec/name (second more)}))))
+(defn keyword [qualified & more]
+  (with-transform-meta
+    #(let [qualified? (= qualified :qualified)]
+       (merge {::parse.spec/kind :keyword}
+              (cond
+                qualified?
+                {::parse.spec/ns (second more)
+                 ::parse.spec/name (nth more 3)}
+                (= (first more) "::")
+                {::parse.spec/ns ::parse.spec/current
+                 ::parse.spec/name (second more)}
+                :else
+                {::parse.spec/name (second more)})))))
 
-(deftransform map [& more]
-  (let [kvs (partition-all 2 more)
-        _ (parse-assert! (every? #(= 2 (count %)) kvs)
-                         parse.binding/*current-form*
-                         "maps require an even number of forms")
-        values (mapv #(do {::parse.spec/key (first %)
-                           ::parse.spec/value (second %)})
-                     kvs)]
-    (single-values :map values)))
+(defn map [& more]
+  (with-transform-meta
+    (fn []
+      (let [kvs (partition-all 2 more)
+            _ (parse-assert! (every? #(= 2 (count %)) kvs)
+                             parse.binding/*current-form*
+                             "maps require an even number of forms")
+            values (mapv (fn [[k v]]
+                           {::parse.spec/key k
+                            ::parse.spec/value v})
+                         kvs)]
+        (single-values :map values)))))
 
-(deftransform set [& more]
-  ; Doesn't go into a set yet, since it needs to be evaluated before it's deduped.
-  (single-values :set (vec more)))
+(defn set [& more]
+  (with-transform-meta
+    ; Doesn't go into a set yet, since it needs to be evaluated before it's deduped.
+    #(single-values :set (vec more))))
 
-(deftransform vector [& more]
-  (single-values :vector (vec more)))
+(defn vector [& more]
+  (with-transform-meta
+    #(single-values :vector (vec more))))
 
-(deftransform identifier [qualification & more]
-  (let [qualified? (= qualification :qualified)]
-    (merge {::parse.spec/kind :identifier}
-           (if qualified?
-             {::parse.spec/ns (first more)
-              ::parse.spec/name (second more)}
-             {::parse.spec/name (first more)}))))
+(defn identifier [qualification & more]
+  (with-transform-meta
+    #(let [qualified? (= qualification :qualified)]
+       (merge {::parse.spec/kind :identifier}
+              (if qualified?
+                {::parse.spec/ns (first more)
+                 ::parse.spec/name (second more)}
+                {::parse.spec/name (first more)})))))
 
-(deftransform def-expression [& more]
-  {::parse.spec/kind :binding
-   ::parse.spec/identifier (first more)
-   ::parse.spec/value (second more)
-   ::parse.spec/scope ::parse.spec/global})
+(defn def-expression [& more]
+  (with-transform-meta
+    #(do
+       {::parse.spec/kind :binding
+        ::parse.spec/identifier (first more)
+        ::parse.spec/value (second more)
+        ::parse.spec/scope ::parse.spec/global})))
 
-(deftransform argument-list [& more]
-  (vec more))
+(defn argument-list [& more]
+  (with-transform-meta
+    #(vec more)))
 
-(deftransform do-expression [& more]
-  (let [ret (last more)]
-    {::parse.spec/kind :do
-     ::parse.spec/body (into [] (butlast more))
-     ::parse.spec/return (if (some? ret)
-                           ret
-                           (constant none :nil))}))
+(defn do-expression [& more]
+  (with-transform-meta
+    #(let [ret (last more)]
+       {::parse.spec/kind :do
+        ::parse.spec/body (into [] (butlast more))
+        ::parse.spec/return (if (some? ret)
+                              ret
+                              (constant none :nil))})))
 
-(deftransform fn-expression [& more]
-  (let [has-name? (= :identifier (-> more first :kind))
-        ; TODO: Add parse support for variadic fns
-        params (mapv (fn [ident]
-                       {::parse.spec/kind :binding
-                        ::parse.spec/identifier ident
-                        ::parse.spec/scope ::parse.spec/parameter})
-                     (if has-name?
-                       (second more)
-                       (first more)))
-        body (if has-name?
-               (drop 2 more)
-               (rest more))]
-    (merge {::parse.spec/kind :fn
-            ::parse.spec/parameters params
-            ::parse.spec/body (apply do-expression body)}
-           (when has-name?
-             {::parse.spec/name {::parse.spec/kind :binding
-                                 ::parse.spec/identifier (first more)
-                                 ::parse.spec/scope ::parse.spec/fn}}))))
+(defn fn-expression [& more]
+  (with-transform-meta
+    #(let [has-name? (= :identifier (-> more first :kind))
+           ; TODO: Add parse support for variadic fns
+           params (mapv (fn [ident]
+                          {::parse.spec/kind :binding
+                           ::parse.spec/identifier ident
+                           ::parse.spec/scope ::parse.spec/parameter})
+                        (if has-name?
+                          (second more)
+                          (first more)))
+           body (if has-name?
+                  (drop 2 more)
+                  (rest more))]
+       (merge {::parse.spec/kind :fn
+               ::parse.spec/parameters params
+               ::parse.spec/body (apply do-expression body)}
+              (when has-name?
+                {::parse.spec/name {::parse.spec/kind :binding
+                                    ::parse.spec/identifier (first more)
+                                    ::parse.spec/scope ::parse.spec/fn}})))))
 
-(deftransform if-expression [& [condition then else]]
-  (merge {::parse.spec/kind :if
-          ::parse.spec/condition condition
-          ::parse.spec/then then}
-         (when (some? else)
-           {::parse.spec/else else})))
+(defn if-expression [& [condition then else]]
+  (with-transform-meta
+    #(merge {::parse.spec/kind :if
+             ::parse.spec/condition condition
+             ::parse.spec/then then}
+            (when (some? else)
+              {::parse.spec/else else}))))
 
-(deftransform let-bindings [& bindings]
-  (->> (partition-all 2 bindings)
-       (mapv (fn [[ident value]]
-               {::parse.spec/kind :binding
-                ::parse.spec/identifier ident
-                ::parse.spec/value value
-                ::parse.spec/scope ::parse.spec/let}))))
+(defn let-bindings [& bindings]
+  (with-transform-meta
+    #(->> (partition-all 2 bindings)
+          (mapv (fn [[ident value]]
+                  {::parse.spec/kind :binding
+                   ::parse.spec/identifier ident
+                   ::parse.spec/value value
+                   ::parse.spec/scope ::parse.spec/let})))))
 
-(deftransform let-expression [bindings & body]
-  {::parse.spec/kind :let
-   ::parse.spec/bindings bindings
-   ::parse.spec/body (apply do-expression body)})
+(defn let-expression [bindings & body]
+  (with-transform-meta
+    #(do
+       {::parse.spec/kind :let
+        ::parse.spec/bindings bindings
+        ::parse.spec/body (apply do-expression body)})))
 
-(deftransform application [& more]
-  {::parse.spec/kind :application
-   ::parse.spec/value (first more)
-   ::parse.spec/arguments (vec (rest more))})
+(defn application [& more]
+  (with-transform-meta
+    #(do
+       {::parse.spec/kind :application
+        ::parse.spec/value (first more)
+        ::parse.spec/arguments (vec (rest more))})))
 
 (def transformer {:nil (partial constant none :nil)
                   :integer (partial constant read-single :integer)
