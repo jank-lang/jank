43655c6a89139d9cc7f4adebde26e5d539e77ee5
jeaye <contact@jeaye.com>
2021-08-14 00:17:48 -0700

    Update deps

--- project.clj
+++ project.clj
@@ -4,23 +4,24 @@
   :license {:name "jank license"
             :url "https://upload.jeaye.com/jank-license"}
   :aliases {"kaocha" ["run" "-m" "kaocha.runner" "--watch"]}
-  :plugins [[lein-cloverage "1.2.1"]]
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/spec.alpha "0.2.187"]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
+            [lein-cloverage "1.2.2"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/spec.alpha "0.2.194"]
                  [org.clojure/test.check "1.1.0"]
                  [instaparse "1.4.10"]
-                 [orchestra "2020.09.18-1"]
-                 [expound "0.8.5"]
+                 [orchestra "2021.01.01-1"]
+                 [expound "0.8.9"]
 
                  ; Colored output
-                 [io.aviso/pretty "0.1.37"]
+                 [io.aviso/pretty "1.1"]
                  [venantius/glow "0.1.6"]]
   :main ^:skip-aot com.jeaye.jank.core
   :target-path "target/%s"
   :resource-paths ["resources/"]
   :profiles {:uberjar {:aot :all
                        :main com.jeaye.jank.core}
-             :dev {:dependencies [[lambdaisland/kaocha "1.0.700"]
+             :dev {:dependencies [[lambdaisland/kaocha "1.0.861"]
                                   [me.raynes/fs "1.4.6"]]
                    :source-paths ["dev/src/"]
                    :resource-paths ["dev/resources/"]
