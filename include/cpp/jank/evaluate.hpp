b97eda9bcf7909c69c6ebf6f8315ce54faacdd35
jeaye <contact@jeaye.com>
2022-12-25 12:07:21 -0800

    Clean up some eval code

--- include/cpp/jank/evaluate.hpp
+++ include/cpp/jank/evaluate.hpp
@@ -5,19 +5,94 @@
 
 namespace jank::evaluate
 {
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expression_ptr const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::def<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::var_deref<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::var_ref<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::call<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::primitive_literal<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::vector<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::map<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::local_reference const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::function<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::recur<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::let<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::do_<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::if_<analyze::expression> const &);
-  runtime::object_ptr eval(runtime::context &, jit::processor const &, analyze::expr::native_raw<analyze::expression> const &);
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expression_ptr const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::def<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::var_deref<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::var_ref<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::call<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::primitive_literal<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::vector<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::map<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::local_reference const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::function<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::recur<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::let<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::do_<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::if_<analyze::expression> const &
+  );
+  runtime::object_ptr eval
+  (
+    runtime::context &,
+    jit::processor const &,
+    analyze::expr::native_raw<analyze::expression> const &
+  );
 }
