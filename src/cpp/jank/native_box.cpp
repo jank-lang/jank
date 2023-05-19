6fb57b64d9c4156ce0d0042680ccc8a6e01a348a
jeaye <contact@jeaye.com>
2023-05-18 22:46:16 -0700

    Add size_t overload for make_box

--- src/cpp/jank/native_box.cpp
+++ src/cpp/jank/native_box.cpp
@@ -7,9 +7,11 @@ namespace jank
   native_box<runtime::obj::boolean> make_box(native_bool const b)
   { return b ? runtime::obj::JANK_TRUE : runtime::obj::JANK_FALSE; }
   native_box<runtime::obj::integer> make_box(int const i)
-  { return make_box<runtime::obj::integer>(native_integer{ i }); }
+  { return make_box<runtime::obj::integer>(static_cast<native_integer>(i)); }
   native_box<runtime::obj::integer> make_box(native_integer const i)
   { return make_box<runtime::obj::integer>(i); }
+  native_box<runtime::obj::integer> make_box(size_t const i)
+  { return make_box<runtime::obj::integer>(static_cast<native_integer>(i)); }
   native_box<runtime::obj::real> make_box(native_real const r)
   { return make_box<runtime::obj::real>(r); }
   native_box<runtime::obj::string> make_box(native_string_view const &s)
