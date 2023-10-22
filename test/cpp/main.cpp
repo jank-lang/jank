9cee841a435c57253a818cc5d7223623fd5cb7cf
jeaye <contact@jeaye.com>
2023-10-21 22:57:17 -0700

    Implement a custom doctest main
    I'm not doing anything different with it, but I've never liked having a
    header-only lib provide my main for me using a custom define. Call me
    old fashioned.

--- test/cpp/main.cpp
+++ test/cpp/main.cpp
@@ -1,2 +1,15 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
+
+int main(int const argc, char const **argv)
+{
+  doctest::Context context;
+  context.applyCommandLine(argc, argv);
+  context.setOption("no-breaks", true);
+
+  auto const res(context.run());
+  if(context.shouldExit())
+  { return res; }
+
+  return res;
+}
