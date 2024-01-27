886abf4f93770716be427e7817d7cb4404b6b191
Saket <saket.is.sam@gmail.com>
2024-01-27 20:06:15 +0530

    feat: allows to execute main in a project

--- src/leiningen/jank.clj
+++ src/leiningen/jank.clj
@@ -1,11 +1,65 @@
 (ns leiningen.jank
   (:require
    [babashka.process :as ps]
+   [babashka.fs :as fs]
    [clojure.pprint :as pp]
    [clojure.string :as string]
    [leiningen.core.classpath :as lcp]
    [leiningen.core.main :as lmain])
   (:import [java.io File]))
+
+(defn- ->absolute-path [path]
+  (if (fs/relative? path)
+    (str (fs/absolutize path))
+    path))
+
+(defn- concat-project-paths [lein-paths jank-paths]
+  (->> jank-paths
+       (map ->absolute-path)
+       (concat lein-paths)))
+
+(defn- prep-jank-project [{jank-project :jank
+                           :as project}]
+  (-> project
+      (dissoc :jank)
+      (update :source-paths #(concat-project-paths % (:source-paths jank-project)))
+      (update :test-paths #(concat-project-paths % (:test-paths jank-project)))
+      (update :resource-paths #(concat-project-paths % (:resource-paths jank-project)))
+      (assoc :compile-path (when-let [compile-path (:compile-path jank-project)]
+                             (->absolute-path compile-path)))
+      (update :dependencies #(concat-project-paths % (:dependencies jank-project)))
+      (assoc :main (:main jank-project))))
+
+(defn- run-main [project classpath & args]
+  (pp/pprint (string/join " " ["jank"
+                               "run-main"
+                               "--class-path"
+                               classpath
+                               "--"
+                               args]))
+  (apply ps/shell
+         {:continue true
+          :dir (:root project)
+          :extra-env (System/getenv)}
+         "jank"
+         "run-main"
+         (:main project)
+         "--class-path"
+         classpath
+         "--"
+         args))
+
+(defn- execute-file [root classpath filename]
+  (if-not filename
+    (lmain/warn "Provide a filename!\nUsage: lein jank run <filename>")
+    (ps/shell {:continue true
+               :dir root
+               :extra-env (System/getenv)}
+              "jank"
+              "run"
+              "--class-path"
+              classpath
+              filename)))
 
 (defn- ^{:help-text "Runs your project or file!"}
   run
@@ -15,20 +69,14 @@
   Make sure `jank` executable is installed and is on the path.\n
   todo(saket): Add support to execute a project using main entrypoint.
   todo(saket): Error handling in case file not found or jank executable not found on path."
-  [project & [filename]]
-  (if-not filename
-    (lmain/warn "Provide a filename!\nUsage: lein jank run <filename>")
-    (let [cp-str (->> project
-                      lcp/get-classpath
-                      (string/join (File/pathSeparatorChar)))]
-      (ps/shell {:continue true
-                 :dir (:root project)
-                 :extra-env (System/getenv)}
-                "jank"
-                "run"
-                "--class-path"
-                cp-str
-                filename))))
+  [project & args]
+  (println (:source-paths project))
+  (let [cp-str (->> project
+                    lcp/get-classpath
+                    (string/join (File/pathSeparatorChar)))]
+    (if (:main project)
+      (apply run-main project cp-str args)
+      (execute-file (:root project) cp-str (get args 0)))))
 
 (declare print-help)
 
@@ -56,4 +104,4 @@
       (do
         (lmain/warn "Invalid subcommand!")
         (print-help))
-      (apply handler project args))))
+      (apply handler (prep-jank-project project) args))))
