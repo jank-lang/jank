1cf1a45ddc098047825ace2192faf551ea7daf19
jeaye <contact@jeaye.com>
2024-05-10 22:55:39 +0000

    Clean up some more lein-jank bits

--- lein-jank/src/leiningen/jank.clj
+++ lein-jank/src/leiningen/jank.clj
@@ -1,27 +1,27 @@
 (ns leiningen.jank
-  (:require
-    [babashka.process :as ps]
-    [babashka.fs :as fs]
-    [clojure.pprint :as pp]
-    [clojure.string :as string]
-    [leiningen.core.classpath :as lcp]
-    [leiningen.core.main :as lmain])
+  (:require [babashka.process :as ps]
+            [babashka.fs :as fs]
+            [clojure.pprint :as pp]
+            [clojure.string :as string]
+            [leiningen.core.classpath :as lcp]
+            [leiningen.core.main :as lmain])
   (:import [java.io File]))
 
-(defn- ->absolute-path [path]
+(defn ->absolute-path [path]
   (if (fs/relative? path)
     (str (fs/absolutize path))
     path))
 
-(defn- concat-project-paths [lein-paths jank-paths]
+(defn concat-project-paths [lein-paths jank-paths]
   (->> jank-paths
        (map ->absolute-path)
        (concat lein-paths)))
 
-(defn- prep-jank-project [{jank-project :jank
-                           :as project}]
+(defn prep-project [{jank-project :jank
+                     :as project}]
   (-> project
       (dissoc :jank)
+      ; TODO: Needed?
       (update :source-paths #(concat-project-paths % (:source-paths jank-project)))
       (update :test-paths #(concat-project-paths % (:test-paths jank-project)))
       (update :resource-paths #(concat-project-paths % (:resource-paths jank-project)))
@@ -30,7 +30,7 @@
       (update :dependencies #(concat-project-paths % (:dependencies jank-project)))
       (assoc :main (:main jank-project))))
 
-(defn- run-main [project classpath & args]
+(defn run-main [project classpath & args]
   (apply ps/shell
          {:continue true
           :dir (:root project)
@@ -43,7 +43,7 @@
          "--"
          args))
 
-(defn- execute-file [root classpath filename]
+(defn execute-file [root classpath filename]
   (if-not filename
     (lmain/warn "Provide a filename!\nUsage: lein jank run <filename>")
     (ps/shell {:continue true
@@ -55,7 +55,7 @@
               classpath
               filename)))
 
-(defn- ^{:help-text "Runs your project or file!"}
+(defn ^{:help-text "Runs your project or file!"}
   run
   "`run` only supports file execution right now i.e. it will execute\n
   jank code in a single file taking into consideration its dependencies.\n
@@ -66,35 +66,29 @@
   [project & args]
   (let [cp-str (->> project
                     lcp/get-classpath
-                    (string/join (File/pathSeparatorChar)))]
+                    (string/join File/pathSeparatorChar))]
     (if (:main project)
       (apply run-main project cp-str args)
       (execute-file (:root project) cp-str (get args 0)))))
 
 (declare print-help)
 
-(def ^:private subtasks
-  {:run #'run
-   :help #'print-help})
+(def subtask-kw->var {:run #'run
+                      :help #'print-help})
 
-(defn- print-help
-  "Prints help about jank subcommands"
-  [& _args]
+(defn print-help [& _args]
   (pp/print-table
     (map (fn [[sub fn-ref]]
            {:sub-command (name sub)
             :help (or (-> fn-ref meta :help-text)
                       (-> fn-ref meta :doc))})
-         subtasks)))
+         subtask-kw->var)))
 
 (defn jank
   "Compile, run and repl into jank"
   [project subcmd & args]
-  (let [handler (->> subcmd
-                     keyword
-                     (get subtasks))]
-    (if-not handler
-      (do
-        (lmain/warn "Invalid subcommand!")
-        (print-help))
-      (apply handler (prep-jank-project project) args))))
+  (if-some [handler (subtask-kw->var (keyword subcmd))]
+    (apply handler (prep-project project) args)
+    (do
+      (lmain/warn "Invalid subcommand!")
+      (print-help))))
