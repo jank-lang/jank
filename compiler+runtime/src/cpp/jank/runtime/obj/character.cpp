14bb3a6f14c9660508298f56af28e7889848634c
jeaye <contact@jeaye.com>
2024-07-15 18:12:28 -0700

    Add a char todo

--- compiler+runtime/src/cpp/jank/runtime/obj/character.cpp
+++ compiler+runtime/src/cpp/jank/runtime/obj/character.cpp
@@ -80,6 +80,7 @@ namespace jank::runtime
 
   void obj::character::to_string(fmt::memory_buffer &buff) const
   {
+    /* TODO: This is actually to_representation, since the string version of \a is just a. */
     fmt::format_to(std::back_inserter(buff), "{}", data);
   }
 
