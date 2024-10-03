d4508cd4c1a0d08993a42077c74a4941ec0e6356
haruki7049 <tontonkirikiri@gmail.com>
2024-10-04 06:02:15 +0900

    feat(default.nix): Init in /comipler+runtime

---
+++ compiler+runtime/default.nix
@@ -0,0 +1,13 @@
+{
+  pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/28b5b8af91ffd2623e995e20aee56510db49001a.tar.gz";
+    sha256 = "09zhy7bj0bd72r8dqpbrnpgapfkg5h91samrv1v8j0qxvv5kgv6n";
+  }) { },
+}:
+
+let
+  stdenv = pkgs.clangStdenv;
+in
+
+stdenv.mkDerivation {
+}
