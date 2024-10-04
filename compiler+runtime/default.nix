7585782c8086b322f3f80a0d3ee273a5244f4a9c
haruki7049 <tontonkirikiri@gmail.com>
2024-10-04 13:22:34 +0900

    chore(default.nix): Edit

--- compiler+runtime/default.nix
+++ compiler+runtime/default.nix
@@ -1,18 +1,12 @@
 {
   pkgs ? import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/28b5b8af91ffd2623e995e20aee56510db49001a.tar.gz";
-    sha256 = "09zhy7bj0bd72r8dqpbrnpgapfkg5h91samrv1v8j0qxvv5kgv6n";
+    url = "https://github.com/NixOS/nixpkgs/archive/75b209227dff3cbfac19f510a62f9446c92beac4.tar.gz";
+    sha256 = "166varzdamn2kkw5zslarzl6026q87wjnss1hlhdsq89hwvfgd2d";
   }) { },
 }:
 
 let
-  #stdenv = pkgs.llvm_19.stdenv;
-  llvm = pkgs.llvm_19.override (oldAttrs: rec {
-    version = "19.1.0";
-    src = oldAttrs.src.overrideAttrs {
-      outputHash = "";
-    };
-  });
+  llvm = pkgs.llvm_19;
   stdenv = llvm.stdenv;
   lib = pkgs.lib;
 in
@@ -24,8 +18,6 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = with pkgs; [
     llvm
-    #llvmPackages_19.clang-unwrapped
-    #llvmPackages_19.clangUseLLVM
     cmake
     git
     ninja
@@ -37,12 +29,9 @@ stdenv.mkDerivation {
     llvmPackages_19.clangUseLLVM
   ];
 
-  CFLAGS = "-B${stdenv.cc.cc}/lib/gcc/${stdenv.targetPlatform.config}/${stdenv.cc.cc.version} -B${stdenv.cc.libc}/lib";
-
   cmakeFlags = [
     "-GNinja"
     "-DDMAKE_BUILD_TYPE=Debug"
-    #"-DDCC_INSTALL_PREFIX=${pkgs.gcc}"
     "-Djank_tests=on"
   ];
 }
