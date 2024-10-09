b7f70301b43e1c20d219d98818cf8d063fc47408
haruki7049 <tontonkirikiri@gmail.com>
2024-10-09 11:49:45 +0900

    style: nix fmt

--- flake.nix
+++ flake.nix
@@ -22,47 +22,49 @@
         inputs.treefmt-nix.flakeModule
       ];
 
-      perSystem = { pkgs, ... }: {
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs.shellcheck.enable = true;
-          programs.nixfmt.enable = true;
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.shellcheck.enable = true;
+            programs.nixfmt.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # Build deps.
+              cmake
+              ninja
+              pkg-config
+              clang
+
+              # Dev tools.
+              entr
+              gcovr
+              lcov
+              git
+              shellcheck
+              # For clangd
+              llvm
+
+              # Libs.
+              boehmgc
+              # TODO: CMake fails to find boost::preprocessor.
+              boost
+              cli11
+              fmt
+              immer
+              # TODO: Doesn't have a nix pkg.
+              #libzippp
+              magic-enum
+              readline
+            ];
+            shellHook = ''
+              export CC="${pkgs.clang}/bin/clang"
+              export CXX="${pkgs.clang}/bin/clang++"
+            '';
+          };
         };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            # Build deps.
-            cmake
-            ninja
-            pkg-config
-            clang
-
-            # Dev tools.
-            entr
-            gcovr
-            lcov
-            git
-            shellcheck
-            # For clangd
-            llvm
-
-            # Libs.
-            boehmgc
-            # TODO: CMake fails to find boost::preprocessor.
-            boost
-            cli11
-            fmt
-            immer
-            # TODO: Doesn't have a nix pkg.
-            #libzippp
-            magic-enum
-            readline
-          ];
-          shellHook = ''
-            export CC="${pkgs.clang}/bin/clang"
-            export CXX="${pkgs.clang}/bin/clang++"
-          '';
-        };
-      };
     };
 }
