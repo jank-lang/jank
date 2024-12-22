aa68d2853da492f695b55f7995e858b1e4256633
jeaye <contact@jeaye.com>
2024-12-21 20:44:44 -0800

    Prevent exceptions from escaping scope_exit

--- compiler+runtime/src/cpp/jank/util/scope_exit.cpp
+++ compiler+runtime/src/cpp/jank/util/scope_exit.cpp
@@ -1,4 +1,9 @@
+#include <fmt/format.h>
+
 #include <jank/util/scope_exit.hpp>
+#include <jank/runtime/object.hpp>
+#include <jank/runtime/core/to_string.hpp>
+#include <jank/native_persistent_string/fmt.hpp>
 
 namespace jank::util
 {
@@ -14,6 +19,19 @@ namespace jank::util
 
   scope_exit::~scope_exit()
   {
-    func();
+    try
+    {
+      func();
+    }
+    catch(runtime::object_ptr const o)
+    {
+      /* TODO: Panic */
+      fmt::println(stderr, "Exception caught in scope_exit: {}", runtime::to_string(o));
+    }
+    catch(std::exception const &e)
+    {
+      /* TODO: Panic */
+      fmt::println(stderr, "Exception caught in scope_exit: {}", e.what());
+    }
   }
 }
