1e86553d1662298ca8d967400a223abac066a042
skykanin <3789764+skykanin@users.noreply.github.com>
2025-01-11 21:56:20 +0100

    add nix flake for building jank
    adds a nix flake which provides a devShell
    with all the packages required to build jank
    (it's not neccessary to build clang/llvm yourself)

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Dev environment for jank";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        ...
+      }: {
+        legacyPackages = pkgs;
+        formatter = pkgs.alejandra;
+        devShells.default = (pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; }) {
+          packages = with pkgs; [
+            # Build deps.
+            cmake
+            ninja
+            pkg-config
+            clang
+
+            # Dev tools.
+            entr
+            gcovr
+            lcov
+            git
+            nixd
+            shellcheck
+            # For clangd
+            llvm
+            llvmPackages.libclang
+
+            # Libs.
+            boehmgc
+            # TODO: CMake fails to find boost::preprocessor.
+            boost
+            cli11
+            fmt
+            immer
+            libzip
+            magic-enum
+            openssl
+            readline
+          ];
+        };
+      };
+    };
+}
