b025137db1b88356c2217f2819c2592d1e283ecb
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-01-29 07:10:00 +0000

    create jar directly for perf

--- compiler+runtime/test/bash/module/jar/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject look-inside-jars "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns look-inside-jars.core})
