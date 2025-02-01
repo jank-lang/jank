1933dd9c373d41d5c98c9343a0c5f8001f17444d
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-01 01:09:41 -0600

    wip

--- compiler+runtime/test/cpp/jank/runtime/obj/persistent_string.cpp
+++ compiler+runtime/test/cpp/jank/runtime/obj/persistent_string.cpp
@@ -1,6 +1,9 @@
 /* clang-format off */
 #include <jank/runtime/obj/persistent_string.hpp>
 #include <jank/runtime/obj/character.hpp>
+#include <jank/runtime/obj/number.hpp>
+#include <jank/runtime/obj/nil.hpp>
+#include <jank/runtime/core.hpp>
 
 /* This must go last; doctest and glog both define CHECK and family. */
 #include <doctest/doctest.h>
@@ -11,28 +14,29 @@ namespace jank
   {
     TEST_CASE("Index")
     {
-      //SUBCASE("Present no default")
-      //{
-      //  runtime::obj::persistent_string const s{ "foo bar" };
-      //  CHECK_EQ(s.get(s, runtime::make_box<runtime::obj::integer>(0)), runtime::make_box<runtime::obj::character>("f"));
-      //}
+      SUBCASE("Present no default")
+      {
+        runtime::obj::persistent_string const s{ "foo bar" };
+        CHECK(runtime::equal(s.get(runtime::make_box<runtime::obj::integer>(0)), (runtime::make_box<runtime::obj::character>("f"))));
+      }
 
-      //SUBCASE("Absent no default")
-      //{
-      //  runtime::obj::persistent_string const s{ "foo bar" };
-      //  CHECK_EQ(s.get(s, runtime::make_box<runtime::obj::integer>(7)), runtime::obj::nil::nil_const());
-      //}
-      //SUBCASE("Present with default")
-      //{
-      //  runtime::obj::persistent_string const s{ "foo bar" };
-      //  CHECK_EQ(s.get(s, runtime::make_box<runtime::obj::integer>(0), runtime::make_box<runtime::obj::character>("o")), runtime::make_box<runtime::obj::character>("f"));
-      //}
+      SUBCASE("Absent no default")
+      {
+        runtime::obj::persistent_string const s{ "foo bar" };
+        CHECK(runtime::equal(s.get(runtime::make_box<runtime::obj::integer>(7)), runtime::obj::nil::nil_const()));
+      }
 
-      //SUBCASE("Absent with default")
-      //{
-      //  runtime::obj::persistent_string const s{ "foo bar" };
-      //  CHECK_EQ(s.get(s, runtime::make_box<runtime::obj::integer>(0), runtime::make_box<runtime::obj::character>("o")), runtime::make_box<runtime::obj::character>("f"));
-      //}
+      SUBCASE("Present with default")
+      {
+        runtime::obj::persistent_string const s{ "foo bar" };
+        CHECK(runtime::equal(s.get(runtime::make_box<runtime::obj::integer>(0), runtime::make_box<runtime::obj::character>("o")), runtime::make_box<runtime::obj::character>("f")));
+      }
+
+      SUBCASE("Absent with default")
+      {
+        runtime::obj::persistent_string const s{ "foo bar" };
+        CHECK(runtime::equal(s.get(runtime::make_box<runtime::obj::integer>(0), runtime::make_box<runtime::obj::character>("o")), runtime::make_box<runtime::obj::character>("f")));
+      }
       //TODO contains
       //TODO get_entry
     }
