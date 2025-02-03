b6ef474055e1e549338405e4a667f2d42d2d111e
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-02 18:20:07 -0600

    vector find test

--- compiler+runtime/test/cpp/jank/runtime/obj/persistent_vector.cpp
+++ compiler+runtime/test/cpp/jank/runtime/obj/persistent_vector.cpp
@@ -57,10 +57,20 @@ namespace jank::runtime::obj
       CHECK(!contains(v, under));
       CHECK(!contains(v, non_int));
     }
-    TEST_CASE("get_entry")
+    TEST_CASE("find")
     {
-      //FIXME
-      //CHECK(equal(find(v, min)), nil);
+      CHECK(
+        equal(find(v, min),
+              persistent_vector::create(nullptr)->conj(make_box(min))->conj(make_box(min_char))));
+      CHECK(
+        equal(find(v, mid),
+              persistent_vector::create(nullptr)->conj(make_box(mid))->conj(make_box(mid_char))));
+      CHECK(
+        equal(find(v, max),
+              persistent_vector::create(nullptr)->conj(make_box(max))->conj(make_box(max_char))));
+      CHECK(equal(find(v, over), nil));
+      CHECK(equal(find(v, under), nil));
+      CHECK(equal(find(v, non_int), nil));
     }
   }
 }
