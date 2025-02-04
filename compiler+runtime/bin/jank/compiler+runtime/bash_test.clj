df402e156de9a96bc0ae66f5b370fa77d2eb1296
jeaye <contact@jeaye.com>
2025-02-04 11:21:38 -0800

    Enable bash test skipping

--- compiler+runtime/bin/jank/compiler+runtime/bash_test.clj
+++ compiler+runtime/bin/jank/compiler+runtime/bash_test.clj
@@ -13,28 +13,31 @@
   (if-not enabled?
     (util/log-info "Not enabled")
     (let [bash-test-dir (str compiler+runtime-dir "/test/bash")
-          test-files (b.f/glob bash-test-dir "**/{pass,fail}-test")
+          test-files (b.f/glob bash-test-dir "**/{pass,fail,skip}-test")
           extra-env {"PATH" (str compiler+runtime-dir "/build" ":" (util/get-env "PATH"))}
           passed? (volatile! true)]
       (doseq [test-file test-files]
         (let [expect-pass? (clojure.string/ends-with? (str test-file) "pass-test")
+              skip? (clojure.string/ends-with? (str test-file) "skip-test")
               dirname (b.f/parent test-file)
               relative-dirname (b.f/relativize bash-test-dir dirname)
               unexpected-result (volatile! nil)]
-          (util/with-elapsed-time duration
-            (let [res @(b.p/process {:out :string
-                                     :err :out
-                                     :dir dirname
-                                     :extra-env extra-env}
-                                    test-file)]
-              (when (and (zero? (:exit res)) expect-pass?)
-                (vreset! unexpected-result res)))
-            (if-some [res @unexpected-result]
-              (do
-                (vreset! passed? false)
-                (println (:out res))
-                (util/log-error-with-time duration "Failed " relative-dirname))
-              (util/log-info-with-time duration "Tested " relative-dirname)))))
+          (if skip?
+            (util/log-warning "Skipped " relative-dirname)
+            (util/with-elapsed-time duration
+              (let [res @(b.p/process {:out :string
+                                       :err :out
+                                       :dir dirname
+                                       :extra-env extra-env}
+                                      test-file)]
+                (when (and (zero? (:exit res)) expect-pass?)
+                  (vreset! unexpected-result res)))
+              (if-some [res @unexpected-result]
+                (do
+                  (vreset! passed? false)
+                  (println (:out res))
+                  (util/log-error-with-time duration "Failed " relative-dirname))
+                (util/log-info-with-time duration "Tested " relative-dirname))))))
 
       (when-not @passed?
         (System/exit 1)))))
