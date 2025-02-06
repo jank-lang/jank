7428e85a62e23f5ca1df28b22225340e19fc2389
jeaye <contact@jeaye.com>
2025-02-05 17:57:44 -0800

    Add graceful lex error handling to highlighting

--- compiler+runtime/src/cpp/jank/ui/highlight.cpp
+++ compiler+runtime/src/cpp/jank/ui/highlight.cpp
@@ -5,6 +5,7 @@
 
 #include <jank/ui/highlight.hpp>
 #include <jank/read/lex.hpp>
+#include <jank/native_persistent_string/fmt.hpp>
 
 namespace jank::ui
 {
@@ -70,28 +71,22 @@ namespace jank::ui
     }
   }
 
+  /* This function will return a map of line numbers to highlighted lines. It gracefully
+   * handles lex errors by not highlighting those tokens and skipping to the next token.
+   * The map will at least contain lines within the range specified and maybe some others. */
   /* TODO: Center horizontally if the line is too long. */
   std::map<size_t, Element>
   highlight(native_persistent_string const &code, size_t const line_start, size_t const line_end)
   {
     read::lex::processor l_prc{ code };
+    auto const end{ l_prc.end() };
     size_t last_offset{}, last_line{ 1 };
     std::map<size_t, Element> lines;
     std::vector<Element> current_line;
+    native_bool ended_on_error{};
 
-    for(auto it(l_prc.begin()); it != l_prc.end(); ++it)
-    {
-      /* TODO: Handle lex errors by showing the rest of the lines without highlighting. */
-
-      auto const &token(it.latest.unwrap().expect_ok());
-      if(token.start.line > line_end)
-      {
-        break;
-      }
-
-      auto const token_size(std::max(token.end.offset - token.start.offset, 1zu));
-      auto const skip(token.start.line < line_start);
-      std::string_view space{ code.data() + last_offset, token.start.offset - last_offset };
+    auto const fill_space([&](native_bool const skip, size_t const offset) {
+      std::string_view space{ code.data() + last_offset, offset - last_offset };
       size_t last_newline{};
       for(auto it(space.find('\n')); it != decltype(space)::npos; it = space.find('\n', it + 1))
       {
@@ -99,7 +94,9 @@ namespace jank::ui
          * space if it's non-empty. */
         if(!skip && last_line >= line_start)
         {
-          current_line.emplace_back(text(" "));
+          std::string line{ space.substr(last_newline, it - last_newline) };
+          //fmt::println("filling in line {} {} '{}'", last_newline, it, line);
+          current_line.emplace_back(text(std::move(line)));
           //current_line.emplace_back(text("<empty>"));
           lines.emplace(last_line, hbox(std::move(current_line)));
           current_line.clear();
@@ -114,7 +111,32 @@ namespace jank::ui
         //current_line.emplace_back(
         //  text(fmt::format("|{}|", std::string{ space.substr(last_newline) })));
       }
+    });
 
+    for(auto it(l_prc.begin()); it != end; ++it)
+    {
+      if(it.latest.unwrap().is_err())
+      {
+        //fmt::println("error {}", it.latest.unwrap().expect_err()->message);
+        ended_on_error = true;
+        continue;
+      }
+      ended_on_error = false;
+
+      auto const &token(it.latest.unwrap().expect_ok());
+      //fmt::println("token {}", read::lex::token_kind_str(token.kind));
+      if(token.start.line > line_end)
+      {
+        /* If we saw an error last, fill in the space until this token. */
+        //fmt::println("past line_end {} > {}", token.start.line, line_end);
+        fill_space(false, token.start.offset);
+        break;
+      }
+
+      auto const skip(token.start.line < line_start);
+      fill_space(skip, token.start.offset);
+
+      auto const token_size(std::max(token.end.offset - token.start.offset, 1zu));
       if(!skip)
       {
         current_line.emplace_back(
@@ -123,6 +145,13 @@ namespace jank::ui
       }
       last_offset = token.start.offset + token_size;
       //fmt::println("last_offset {}", last_offset);
+    }
+
+    /* If we saw an error last, fill in the space until the end of the file. */
+    if(ended_on_error)
+    {
+      //fmt::println("ended on error. filling in space");
+      fill_space(false, code.size());
     }
 
     lines.emplace(last_line, hbox(std::move(current_line)));
