a059c689e090984d299bac358df5b68f4ede3e5f
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-07 00:48:07 -0600

    test core::equal

---
+++ compiler+runtime/test/cpp/jank/runtime/core.cpp
@@ -0,0 +1,21 @@
+#include <jank/runtime/obj/repeat.hpp>
+#include <jank/runtime/core.hpp>
+#include <jank/runtime/core/make_box.hpp>
+
+/* This must go last; doctest and glog both define CHECK and family. */
+#include <doctest/doctest.h>
+
+namespace jank::runtime
+{
+  TEST_SUITE("repeat")
+  {
+    TEST_CASE("equal")
+    {
+      CHECK(equal(make_box(42), make_box(42)));
+      CHECK(equal(nullptr, nullptr));
+      CHECK(!equal(nullptr, make_box(42)));
+      CHECK(!equal(make_box(42), nullptr));
+    }
+  }
+}
+
---
+++ compiler+runtime/test/cpp/jank/runtime/obj/repeat.cpp
@@ -0,0 +1,24 @@
+#include <jank/runtime/obj/repeat.hpp>
+#include <jank/runtime/core.hpp>
+#include <jank/runtime/core/make_box.hpp>
+
+/* This must go last; doctest and glog both define CHECK and family. */
+#include <doctest/doctest.h>
+
+namespace jank::runtime::obj
+{
+  TEST_SUITE("repeat")
+  {
+    TEST_CASE("equal")
+    {
+      CHECK(equal(make_box<repeat>(make_box(5), make_box(5)), make_box<repeat>(make_box(5), make_box(5))));
+      CHECK(equal(make_box<repeat>(make_box(1), make_box(1)), make_box<repeat>(make_box(1), make_box(1))));
+      CHECK(!equal(make_box<repeat>(make_box(6), make_box(5)), make_box<repeat>(make_box(5), make_box(5))));
+      CHECK(!equal(make_box<repeat>(make_box(5), make_box(5)), make_box<repeat>(make_box(5), make_box(6))));
+      CHECK(equal(make_box<repeat>(make_box(0), make_box(0)), make_box<repeat>(make_box(0), make_box(0))));
+      CHECK(!equal(make_box<repeat>(make_box(0), make_box(0)), make_box<repeat>(make_box(5), make_box(0))));
+      //TODO empty repeat uses persistent_list
+      //CHECK(!equal(make_box<repeat>(make_box(1), make_box(1)), make_box<repeat>(make_box(0), (make_box(1)))));
+    }
+  }
+}
