9a53a23f5c53017a5cd415000214511086c90b64
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-25 14:04:22 -0600

    rm tests

--- compiler+runtime/test/cpp/jank/runtime/core.cpp
+++ compiler+runtime/test/cpp/jank/runtime/core.cpp
@@ -1,5 +1,4 @@
 #include <jank/runtime/core.hpp>
-#include <jank/runtime/obj/persistent_string.hpp>
 #include <jank/runtime/core/make_box.hpp>
 
 /* This must go last; doctest and glog both define CHECK and family. */
@@ -9,20 +8,6 @@ namespace jank::runtime
 {
   TEST_SUITE("core runtime")
   {
-    TEST_CASE("subs")
-    {
-      auto const s{ make_box<obj::persistent_string>("foo bar") };
-      CHECK(equal(subs(s, make_box(0)), make_box<obj::persistent_string>("foo bar")));
-      CHECK(equal(subs(s, make_box(1)), make_box<obj::persistent_string>("oo bar")));
-      CHECK(equal(subs(s, make_box(2)), make_box<obj::persistent_string>("o bar")));
-      CHECK(equal(subs(s, make_box(0), make_box(7)), make_box<obj::persistent_string>("foo bar")));
-      CHECK(equal(subs(s, make_box(0), make_box(0)), make_box<obj::persistent_string>("")));
-      CHECK(equal(subs(s, make_box(1), make_box(1)), make_box<obj::persistent_string>("")));
-      CHECK(equal(subs(s, make_box(1), make_box(6)), make_box<obj::persistent_string>("oo ba")));
-      CHECK(equal(subs(s, make_box(1), make_box(7)), make_box<obj::persistent_string>("oo bar")));
-      CHECK(equal(subs(s, make_box(3), make_box(4)), make_box<obj::persistent_string>(" ")));
-    }
-
     TEST_CASE("equal")
     {
       CHECK(equal(nullptr, nullptr));
