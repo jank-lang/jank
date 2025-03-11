f04683c7f3f3bb1b03b786d7906f2d3462a80ea7
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2025-03-11 19:42:26 +0100

    fix behavior of nth with default argument (#294)
    * Handle fallback for nth
    
    Closes #275
    
    * merge branches

--- compiler+runtime/src/cpp/jank/runtime/core/seq.cpp
+++ compiler+runtime/src/cpp/jank/runtime/core/seq.cpp
@@ -820,13 +820,9 @@ namespace jank::runtime
   object_ptr nth(object_ptr const o, object_ptr const idx, object_ptr const fallback)
   {
     auto const index(to_int(idx));
-    if(index < 0)
+    if(index < 0 || o == obj::nil::nil_const())
     {
       return fallback;
-    }
-    else if(o == obj::nil::nil_const())
-    {
-      return o;
     }
 
     return visit_object(
