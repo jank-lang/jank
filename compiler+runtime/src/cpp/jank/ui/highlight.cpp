5a00f0b2bb0a97060030e3ad092c78da020797da
jeaye <contact@jeaye.com>
2025-05-18 16:19:40 -0700

    Fix multi-line token highlighting

--- compiler+runtime/src/cpp/jank/ui/highlight.cpp
+++ compiler+runtime/src/cpp/jank/ui/highlight.cpp
@@ -3,6 +3,7 @@
 #include <jank/ui/highlight.hpp>
 #include <jank/read/lex.hpp>
 #include <jank/runtime/module/loader.hpp>
+#include <jank/util/fmt/print.hpp>
 
 namespace jank::ui
 {
@@ -14,17 +15,17 @@ namespace jank::ui
     "if",  "quote", "var", "try",  "catch", "finally", "throw", "letfn*",
   };
 
-  static Element symbol_color(Element const &e, native_persistent_string_view const &sym)
+  static Decorator symbol_color(native_persistent_string_view const &sym)
   {
     if(specials.contains(sym))
     {
-      return e | color(Color::CyanLight) | bold;
+      return color(Color::CyanLight) | bold;
     }
 
-    return e | color(Color::Default);
+    return color(Color::Default);
   }
 
-  static Element token_color(Element const &e, read::lex::token const &token)
+  static Decorator token_color(read::lex::token const &token)
   {
     using namespace jank::read::lex;
     switch(token.kind)
@@ -46,26 +47,25 @@ namespace jank::ui
       case token_kind::unquote_splice:
       case token_kind::deref:
       case token_kind::nil:
-        return e | color(Color::DarkOrange);
+        return color(Color::DarkOrange);
       case token_kind::keyword:
-        return e | color(Color::BlueLight);
+        return color(Color::BlueLight);
       case token_kind::comment:
-        return e | color(Color::GrayLight);
+        return color(Color::GrayLight);
       case token_kind::integer:
       case token_kind::real:
       case token_kind::ratio:
       case token_kind::big_integer:
       case token_kind::boolean:
       case token_kind::character:
-        return e | color(Color::MagentaLight);
+        return color(Color::MagentaLight);
       case token_kind::string:
       case token_kind::escaped_string:
-        return e | color(Color::GreenLight);
+        return color(Color::GreenLight);
       case token_kind::symbol:
-        return symbol_color(e, std::get<native_persistent_string_view>(token.data));
+        return symbol_color(std::get<native_persistent_string_view>(token.data));
       case token_kind::eof:
-        return e | color(Color::Default);
-        break;
+        return color(Color::Default);
     }
   }
 
@@ -83,17 +83,19 @@ namespace jank::ui
     bool ended_on_error{};
     static auto const config{ FlexboxConfig().SetGap(0, 0) };
 
-    auto const fill_space([&](bool const skip, usize const offset) {
+    /* As we progress through the file, we update our offset. When we call `fill_in_lines`, we
+     * catch up based on scanning for new line characters between the last offset and the
+     * new offset. For any new lines found, we track the line and build it up if it's
+     * in our target range. */
+    auto const fill_in_lines([&](bool const skip, usize const offset, Decorator const &color) {
       std::string_view const space{ code.data() + last_offset, offset - last_offset };
       usize last_newline{};
       for(auto it(space.find('\n')); it != decltype(space)::npos; it = space.find('\n', it + 1))
       {
-        /* We add a space since this could be an empty line and ftxui will only make it take
-         * space if it's non-empty. */
         if(!skip && last_line >= line_start)
         {
           std::string line{ space.substr(last_newline, it - last_newline) };
-          current_line.emplace_back(text(std::move(line)));
+          current_line.emplace_back(text(std::move(line)) | color);
           lines.emplace(last_line, flexbox(std::move(current_line), config));
           current_line.clear();
         }
@@ -102,7 +104,7 @@ namespace jank::ui
       }
       if(!skip && last_newline < space.size())
       {
-        current_line.emplace_back(text(std::string{ space.substr(last_newline) }));
+        current_line.emplace_back(text(std::string{ space.substr(last_newline) }) | color);
       }
     });
 
@@ -118,21 +120,37 @@ namespace jank::ui
       auto const &token(it.latest.unwrap().expect_ok());
       if(token.start.line > line_end)
       {
-        /* If we saw an error last, fill in the space until this token. */
-        fill_space(false, token.start.offset);
+        /* We're at the end of our range. In case we saw an error last, fill in the space
+         * until this token. */
+        fill_in_lines(false, token.start.offset, color(Color::Default));
         break;
       }
 
-      auto const skip(token.start.line < line_start);
-      fill_space(skip, token.start.offset);
+      auto const skip(token.end.line < line_start);
+      fill_in_lines(skip, token.start.offset, color(Color::Default));
+      last_offset = token.start.offset;
 
       /* TODO: Large tokens can be broken up further, to aid in line wrapping. For example,
        * using `paragraph` for comments. */
       auto const token_size(std::max(token.end.offset - token.start.offset, 1llu));
       if(!skip)
       {
-        current_line.emplace_back(
-          token_color(text(std::string{ code.data() + token.start.offset, token_size }), token));
+        native_persistent_string_view const code_range{ code.data() + token.start.offset,
+                                                        token_size };
+        /* Multi-line tokens can't just be added to the current line. We need to walk through
+         * all of the new lines and build things up accordingly. We just use the same
+         * `fill_in_lines` fn for this, but we give it the token color.
+         *
+         * This only adds lines if it finds a new line character, though, so the normal
+         * case of a single-line token is handled below. */
+        if(code_range.find('\n') != native_persistent_string_view::npos)
+        {
+          fill_in_lines(skip, token.start.offset + token_size, token_color(token));
+        }
+        else
+        {
+          current_line.emplace_back(text(std::string{ code_range }) | token_color(token));
+        }
       }
       last_offset = token.start.offset + token_size;
     }
@@ -140,7 +158,7 @@ namespace jank::ui
     /* If we saw an error last, fill in the space until the end of the file. */
     if(ended_on_error)
     {
-      fill_space(false, code.size());
+      fill_in_lines(false, code.size(), color(Color::Default));
     }
 
     lines.emplace(last_line, flexbox(std::move(current_line), config));
