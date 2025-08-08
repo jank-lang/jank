ce44bc23a9113b383c6fc05961e4521cf28156a7
jeaye <contact@jeaye.com>
2025-08-08 15:56:34 -0700

    Disable bash tests in CI

--- compiler+runtime/bin/jank/compiler+runtime/core.clj
+++ compiler+runtime/bin/jank/compiler+runtime/core.clj
@@ -22,7 +22,7 @@
                                            :coverage (util/get-env "JANK_COVERAGE" "off")})
 
   ; Bash tests
-  (jank.compiler+runtime.bash-test/-main {:enabled? build?})
+  #_(jank.compiler+runtime.bash-test/-main {:enabled? build?})
 
   ; Codecov (merge results)
   (jank.compiler+runtime.coverage/-main {:enabled? (= "on" (util/get-env "JANK_COVERAGE" "off"))}))
