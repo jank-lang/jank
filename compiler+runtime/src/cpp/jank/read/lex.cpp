1002c5b09d8716f5722b0e5a98e4c16d50acf9ac
Emma Griffin <emma.audrey.g@gmail.com>
2025-08-15 21:06:35 -0600

    Take 1

--- compiler+runtime/src/cpp/jank/read/lex.cpp
+++ compiler+runtime/src/cpp/jank/read/lex.cpp
@@ -574,7 +574,7 @@ namespace jank::read::lex
           {
             return ch.expect_err();
           }
-          else if(std::iswspace(static_cast<int>(ch.expect_ok().character)))
+          else if(ch.expect_ok().character != ' ' && std::iswspace(static_cast<int>(ch.expect_ok().character)))
           {
             return error::lex_incomplete_character("A \\ must be followed by a character value.",
                                                    { token_start, pos });
