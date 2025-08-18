a1b21772cd9e884bf4d88386175b256efab81700
jeaye <contact@jeaye.com>
2025-08-18 13:09:19 -0700

    Add final newline to deb control file

--- compiler+runtime/bin/jank/compiler+runtime/package.clj
+++ compiler+runtime/bin/jank/compiler+runtime/package.clj
@@ -20,7 +20,8 @@ Version: %s
 Architecture: amd64
 Maintainer: Jeaye Wilkerson <jeaye@jank-lang.org>
 Depends: libssl-dev, gcc, libzip-dev, libxml2-dev, libstdc++-14-dev
-Description: The native Clojure dialect hosted on LLVM with seamless C++ interop." version)]
+Description: The native Clojure dialect hosted on LLVM with seamless C++ interop.
+" version)]
         (util/quiet-shell {:dir compiler+runtime-dir
                            :extra-env {"DESTDIR" dir}}
                           "./bin/install")
