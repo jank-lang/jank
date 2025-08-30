a5db94fa095b13c5feac37ac5452e48bb71e24f4
Saket Patel <saket.is.sam@gmail.com>
2025-08-30 13:18:23 +0530

    fix: `output_filename` to write module conflicts with AOT compiled output_filename

--- compiler+runtime/src/cpp/jank/runtime/context.cpp
+++ compiler+runtime/src/cpp/jank/runtime/context.cpp
@@ -372,9 +372,9 @@ namespace jank::runtime
   {
     profile::timer const timer{ util::format("write_module {}", module_name) };
     std::filesystem::path const module_path{
-      util::cli::opts.output_filename.empty()
+      util::cli::opts.output_object_filename.empty()
         ? util::format("{}/{}.o", binary_cache_dir, module::module_to_path(module_name))
-        : jtl::immutable_string{ util::cli::opts.output_filename }
+        : jtl::immutable_string{ util::cli::opts.output_object_filename }
     };
     std::filesystem::create_directories(module_path.parent_path());
 
