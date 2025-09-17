6f0c4850b6a2008f4617151813420f18b63452d4
jeaye <contact@jeaye.com>
2025-09-16 21:05:44 -0700

    Re-enable bash tests

--- compiler+runtime/bin/jank/compiler+runtime/core.clj
+++ compiler+runtime/bin/jank/compiler+runtime/core.clj
@@ -23,9 +23,7 @@
                                            :coverage (util/get-env "JANK_COVERAGE" "off")})
 
   ; Bash tests
-  ; TODO: Enable when precompiled headers can be correctly accessed
-  ; for AOT compiled binaries
-  #_(jank.compiler+runtime.bash-test/-main {:enabled? build?})
+  (jank.compiler+runtime.bash-test/-main {:enabled? build?})
 
   ; Codecov (merge results)
   (jank.compiler+runtime.coverage/-main {:enabled? (= "on" (util/get-env "JANK_COVERAGE" "off"))})
