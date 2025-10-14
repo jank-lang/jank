1ecc48a1044bc6a96bbf1b4cd084f3d88193f85c
jeaye <contact@jeaye.com>
2025-10-13 17:04:15 -0700

    Disable e2e tests

--- compiler+runtime/bin/jank/compiler+runtime/core.clj
+++ compiler+runtime/bin/jank/compiler+runtime/core.clj
@@ -23,7 +23,7 @@
                                            :coverage (util/get-env "JANK_COVERAGE" "off")})
 
   ; Bash tests
-  (jank.compiler+runtime.bash-test/-main {:enabled? (and build?
+  #_(jank.compiler+runtime.bash-test/-main {:enabled? (and build?
                                                          ; AOT compilation fails when sanitizers are enabled.
                                                          (= "none" (util/get-env "JANK_SANITIZE" "none")))})
 
