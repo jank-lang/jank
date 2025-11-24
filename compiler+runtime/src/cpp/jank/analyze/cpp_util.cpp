381a8bba4e3ca1551f9e14789606adba02ea84c8
jeaye <contact@jeaye.com>
2025-11-24 13:05:03 -0800

    Bypass one layer of typedefs manually

--- compiler+runtime/src/cpp/jank/analyze/cpp_util.cpp
+++ compiler+runtime/src/cpp/jank/analyze/cpp_util.cpp
@@ -300,6 +300,26 @@ namespace jank::analyze::cpp_util
     }
     /* TODO: Handle typed object refs, too. */
 
+    /* TODO: We probably want a recursive approach to this, for types and scopes. */
+    auto const qual_type{ clang::QualType::getFromOpaquePtr(type) };
+    if(auto const *alias{
+         llvm::dyn_cast_or_null<clang::TypedefType>(qual_type.getTypePtrOrNull()) };
+       alias)
+    {
+      if(auto const *alias_decl{ alias->getDecl() }; alias_decl)
+      {
+        auto alias_name{ alias_decl->getQualifiedNameAsString() };
+        if(!alias_name.empty())
+        {
+          if(Cpp::IsPointerType(type))
+          {
+            alias_name += "*";
+          }
+          return alias_name;
+        }
+      }
+    }
+
     if(auto const scope{ Cpp::GetScopeFromType(type) }; scope)
     {
       auto name{ get_qualified_name(scope) };
