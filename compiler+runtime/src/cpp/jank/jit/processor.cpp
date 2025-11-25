2d10bf8f2a12c1231397a7f3897fd75d798d39cd
Cameron Barre <cjbarre@gmail.com>
2025-11-24 18:33:39 -0800

    Add clang args that fix the potential for OS and local Clang build std libs conflicting with each other. (#602)
    * Add clang args that fix the potential for OS and local Clang build std libs conflicting with each other.
    
    * Use the new flags conditionally on mac-like platforms.

--- compiler+runtime/src/cpp/jank/jit/processor.cpp
+++ compiler+runtime/src/cpp/jank/jit/processor.cpp
@@ -120,8 +120,18 @@ namespace jank::jit
       throw error::system_clang_executable_not_found();
     }
     auto const clang_dir{ std::filesystem::path{ clang_path_str.unwrap().c_str() }.parent_path() };
-    args.emplace_back("-I");
-    args.emplace_back(strdup((clang_dir / "../include").c_str()));
+
+    if constexpr(jtl::current_platform == jtl::platform::macos_like)
+    {
+      args.emplace_back("-nostdinc++");
+      args.emplace_back("-isystem");
+      args.emplace_back(strdup((clang_dir / "../include/c++/v1").c_str()));
+    }
+    else
+    {
+      args.emplace_back("-I");
+      args.emplace_back(strdup((clang_dir / "../include").c_str()));
+    }
 
     auto const clang_resource_dir{ util::find_clang_resource_dir() };
     if(clang_resource_dir.is_none())
