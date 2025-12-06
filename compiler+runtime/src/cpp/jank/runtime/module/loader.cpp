cefda59642cf314cd733a6afaf07d0b5951885b1
Saket <saket.is.sam@gmail.com>
2025-12-06 12:20:15 +0530

    test(module-loader): test for cpp loading and preference checks
    .jank overrides .cljc overrides .cpp based modules

--- compiler+runtime/src/cpp/jank/runtime/module/loader.cpp
+++ compiler+runtime/src/cpp/jank/runtime/module/loader.cpp
@@ -798,10 +798,6 @@ namespace jank::runtime::module
           return find_result{ entry, module_type };
         }
       }
-      else if(entry.cpp.is_some())
-      {
-        return find_result{ entry, module_type::cpp };
-      }
       else if(entry.jank.is_some())
       {
         return find_result{ entry, module_type::jank };
@@ -809,6 +805,10 @@ namespace jank::runtime::module
       else if(entry.cljc.is_some())
       {
         return find_result{ entry, module_type::cljc };
+      }
+      else if(entry.cpp.is_some())
+      {
+        return find_result{ entry, module_type::cpp };
       }
     }
 
---
+++ compiler+runtime/test/bash/module/cpp/src/jank_test/cljc.cljc
@@ -0,0 +1,2 @@
+(ns jank-test.cljc)
+(defn -main [] #?(:clj :clj :jank :jank :default :default))
