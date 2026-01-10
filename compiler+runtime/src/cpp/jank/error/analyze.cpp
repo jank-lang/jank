f8e0445c482571bdd54080c85649259fa89edf94
Jeaye Wilkerson <contact@jeaye.com>
2026-01-09 18:09:46 -0800

    Add `cpp/unsafe-cast` (#656)

--- compiler+runtime/src/cpp/jank/error/analyze.cpp
+++ compiler+runtime/src/cpp/jank/error/analyze.cpp
@@ -329,6 +329,29 @@ namespace jank::error
     return make_error(kind::analyze_invalid_cpp_cast, message, source, expansion);
   }
 
+  error_ref analyze_invalid_cpp_cast(jtl::immutable_string const &message,
+                                     read::source const &source,
+                                     error::note const &note,
+                                     runtime::object_ref const expansion)
+  {
+    return make_error(kind::analyze_invalid_cpp_cast, message, source, note, expansion);
+  }
+
+  error_ref analyze_invalid_cpp_unsafe_cast(jtl::immutable_string const &message,
+                                            read::source const &source,
+                                            runtime::object_ref const expansion)
+  {
+    return make_error(kind::analyze_invalid_cpp_unsafe_cast, message, source, expansion);
+  }
+
+  error_ref analyze_invalid_cpp_unsafe_cast(jtl::immutable_string const &message,
+                                            read::source const &source,
+                                            error::note const &note,
+                                            runtime::object_ref const expansion)
+  {
+    return make_error(kind::analyze_invalid_cpp_unsafe_cast, message, source, note, expansion);
+  }
+
   error_ref analyze_invalid_cpp_box(jtl::immutable_string const &message,
                                     read::source const &source,
                                     runtime::object_ref const expansion)
