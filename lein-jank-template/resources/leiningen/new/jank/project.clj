f2da6b9f3ed099b5a7587be147650a51be895bc9
jeaye <contact@jeaye.com>
2026-02-19 15:56:20 -0800

    Bump lein-jank to 0.5

--- lein-jank-template/resources/leiningen/new/jank/project.clj
+++ lein-jank-template/resources/leiningen/new/jank/project.clj
@@ -2,7 +2,7 @@
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies []
-  :plugins [[org.jank-lang/lein-jank "0.4"]]
+  :plugins [[org.jank-lang/lein-jank "0.5"]]
   :middleware [leiningen.jank/middleware]
   :main {{namespace}}
   :profiles {:base {:jank {:output-dir "target/debug"
