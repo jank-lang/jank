1786c4dad6d918da2cb20d1765eb6c72ca260182
jeaye <contact@jeaye.com>
2026-06-16 13:01:02 -0700

    Fix standalone lib installation

--- compiler+runtime/cmake/install.cmake
+++ compiler+runtime/cmake/install.cmake
@@ -1,7 +1,13 @@
 # We don't have any dynamic lib deps which are installed alongside jank.
 set(CMAKE_SKIP_INSTALL_RPATH ON)
 install(TARGETS jank_exe_phase_2 DESTINATION bin)
-install(FILES ${CMAKE_BINARY_DIR}/libjank-standalone.a DESTINATION lib/jank/${PROJECT_VERSION}/lib)
+install(
+  FILES
+  ${CMAKE_BINARY_DIR}/libjank-static-runtime.a
+  ${CMAKE_BINARY_DIR}/libjank-dynamic-runtime.a
+  DESTINATION
+  lib/jank/${PROJECT_VERSION}/lib
+)
 
 function(install_symlink target link_name dir)
   install(CODE "
