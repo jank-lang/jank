82110fc2646599ecef8a1c85529cc8a06dfc9033
jeaye <contact@jeaye.com>
2026-06-23 21:21:06 -0700

    Bump lein-jank version in template

--- lein-jank-template/resources/leiningen/new/jank/project.clj
+++ lein-jank-template/resources/leiningen/new/jank/project.clj
@@ -2,7 +2,7 @@
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies []
-  :plugins [[org.jank-lang/lein-jank "0.7"]]
+  :plugins [[org.jank-lang/lein-jank "2026.06-1"]]
   :middleware [leiningen.jank/middleware]
   :main {{namespace}}
   :profiles {:base {:jank {:target-dir "target/debug"
