8d6ee2c0f74cc669d9e3f448c2008f6af5fe7de8
Kyle Cesare <kcesare@gmail.com>
2026-07-15 19:04:54 -0600

    Add rerun-if[-env]-changed to jank-build (#886)
    * Add rerun-if[-env]-changed to jank-build
    
    * Clean up change detection + tests
    
    * Use LATEST in example projects.
    
    * Remove unnecessary select-keys
    
    * Track all source paths by default

--- lein-jank/test-data/test-parent/project.clj
+++ lein-jank/test-data/test-parent/project.clj
@@ -3,6 +3,6 @@
   :url "https://example.com/FIXME"
   :license {:name "MPL 2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :plugins [[org.jank-lang/lein-jank "0.7"]]
+  :plugins [[org.jank-lang/lein-jank "LATEST"]]
   :middleware [leiningen.jank/middleware]
   :dependencies [[org.jank-lang/test-child "0.1.0"]])
