f06d7032cec3c14ca3bee113dd72028669d6c84c
jeaye <contact@jeaye.com>
2026-07-22 11:56:36 -0700

    Fix declarative flag warning

--- lein-jank-template/resources/leiningen/new/jank/project.clj
+++ lein-jank-template/resources/leiningen/new/jank/project.clj
@@ -2,7 +2,7 @@
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies []
-  :plugins [[org.jank-lang/lein-jank "2026.07-2"]]
+  :plugins [[org.jank-lang/lein-jank "2026.07-3"]]
   :middleware [leiningen.jank/middleware]
   :main {{namespace}}
   :profiles {:base {:jank {:target-dir "target/debug"
