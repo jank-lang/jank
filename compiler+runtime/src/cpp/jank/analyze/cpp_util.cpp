206dc071461a3edefedb675a4f9a9130027ff873
jeaye <contact@jeaye.com>
2026-07-24 16:12:25 -0700

    Go back to long

--- compiler+runtime/src/cpp/jank/analyze/cpp_util.cpp
+++ compiler+runtime/src/cpp/jank/analyze/cpp_util.cpp
@@ -442,9 +442,9 @@ namespace jank::analyze::cpp_util
     return type;
   }
 
-  jtl::ptr<void> long_long_type()
+  jtl::ptr<void> long_type()
   {
-    static auto const type{ Cpp::GetType("long long") };
+    static auto const type{ Cpp::GetType("long") };
     return type;
   }
 
@@ -566,7 +566,7 @@ namespace jank::analyze::cpp_util
               static auto const type{ int_type() };
               return type;
             }
-            static auto const type{ long_long_type() };
+            static auto const type{ long_type() };
             return type;
           }
           else
