dfcb59026e192db579d407c0fc56a84d41810092
Kyle Cesare <kcesare@gmail.com>
2026-07-24 20:11:25 -0600

    Enable AOT compilation for lein-jank

--- lein-jank/project.clj
+++ lein-jank/project.clj
@@ -2,11 +2,24 @@
   :description "Build your jank projects using Leiningen."
   :url "https://jank-lang.org/"
   :license {:name "MPL 2.0"
-            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+            :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[babashka/fs "0.5.33"]
                  [babashka/process "0.6.25"]
                  [leiningen-core "2.12.0"]
                  [org.clojure/tools.cli "1.4.256"]
                  [org.clojure/tools.namespace "1.5.1"]]
-  :profiles {:dev {:dependencies [[nubank/matcher-combinators "3.10.0"]]}}
-  :eval-in-leiningen true)
+  :profiles {:dev      {:dependencies      [[nubank/matcher-combinators "3.10.0"]]
+                        :eval-in-leiningen :true}
+             :provided {:dependencies [[org.clojure/clojure "1.12.5"]]}
+             :release  {:aot [jank-build.core
+                              jank-build.change-detection
+                              jank-build.fingerprint
+                              jank-build.util
+                              jank-build.sandbox.core
+                              jank-build.sandbox.bwrap
+                              ;; TODO: fails to AOT compile
+                              ;; leiningen.jank
+                              leiningen.jank.core
+                              leiningen.jank.discovery
+                              leiningen.jank.resolve
+                              leiningen.jank.test]}})
