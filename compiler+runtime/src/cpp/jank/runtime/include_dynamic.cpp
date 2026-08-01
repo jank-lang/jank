867395ef3cde5569da5d537f3036d7912f66dcc9
Chris Badahdah <contact@djblue.dev>
2026-07-31 19:49:15 -0700

    Add `include-header` macro

---
+++ compiler+runtime/src/cpp/jank/runtime/include_dynamic.cpp
@@ -0,0 +1,43 @@
+#include <jank/util/fmt.hpp>
+
+#include <jank/runtime/context.hpp>
+#include <jank/runtime/include.hpp>
+#include <jank/runtime/obj/persistent_list.hpp>
+#include <jank/runtime/obj/persistent_string.hpp>
+#include <jank/runtime/obj/symbol.hpp>
+
+namespace jank::runtime
+{
+  static obj::persistent_list_ref cpp_raw_include(jtl::immutable_string const &header)
+  {
+    static auto const raw(make_box<obj::symbol>("cpp/raw"));
+
+    auto const cpp(header.starts_with("./") ? util::format("#include \"{}\"", header)
+                                            : util::format("#include <{}>", header));
+
+    return make_box<obj::persistent_list>(std::in_place,
+                                          raw,
+                                          make_box<obj::persistent_string>(cpp));
+  }
+
+  obj::persistent_list_ref include_header(jtl::immutable_string const &header)
+  {
+    static auto const _do(make_box<obj::symbol>("do"));
+    static auto const load_header_symbol(make_box<obj::symbol>("cpp/jank.runtime.load_header"));
+
+    auto const load_header_form(
+      make_box<obj::persistent_list>(std::in_place,
+                                     load_header_symbol,
+                                     make_box<obj::persistent_string>(header)));
+
+    return make_box<obj::persistent_list>(std::in_place,
+                                          _do,
+                                          cpp_raw_include(header),
+                                          load_header_form);
+  }
+
+  void load_header(jtl::immutable_string const &header)
+  {
+    __rt_ctx->eval(cpp_raw_include(header));
+  }
+}
