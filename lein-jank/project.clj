6eba6d30dba4062d6badc69696fcebf8beb8560c
jeaye <contact@jeaye.com>
2026-08-28 13:12:49 -0700

    Bump lein-jank version

--- lein-jank/project.clj
+++ lein-jank/project.clj
@@ -1,4 +1,4 @@
-(defproject org.jank-lang/lein-jank "2026.07-3"
+(defproject org.jank-lang/lein-jank "2026.08-1"
   :description "Build your jank projects using Leiningen."
   :url "https://jank-lang.org/"
   :license {:name "MPL 2.0"
