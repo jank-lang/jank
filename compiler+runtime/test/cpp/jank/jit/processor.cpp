1d5dd6b17a3521cca1f10ec89fb7174b3b50df24
jeaye <contact@jeaye.com>
2026-09-13 12:05:59 -0700

    Fix a Windows issue

--- compiler+runtime/test/cpp/jank/jit/processor.cpp
+++ compiler+runtime/test/cpp/jank/jit/processor.cpp
@@ -80,14 +80,15 @@ namespace jank::jit
         auto const expect_failure(filename.starts_with("fail-"));
         auto const expect_throw(filename.starts_with("throw-"));
         auto const allow_failure(filename.starts_with("warn-"));
-        auto const skip(filename.starts_with("skip-"));
+        /* NOLINTNEXTLINE(misc-const-correctness): It's used on Windows. */
+        auto skip(filename.starts_with("skip-"));
         CHECK_MESSAGE((expect_success || expect_failure || allow_failure || expect_throw || skip),
                       "Test file needs to begin with pass- or fail- or throw- or warn- or skip-: ",
                       filename);
         ++test_count;
 #ifdef JANK_WINDOWS_LIKE
-        // skip tests mentioned in file.
-        static auto const windows_skips = [] {
+        /* Skip the tests mentioned in file. */
+        static auto const windows_skips{ [] {
           std::unordered_set<std::string> s;
           std::ifstream infile("test/jit_windows_skips.txt");
           for(std::string line; std::getline(infile, line);)
@@ -99,7 +100,7 @@ namespace jank::jit
             }
           }
           return s;
-        }();
+        }() };
         if(windows_skips.contains(filename))
         {
           skip = true;
